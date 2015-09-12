3ec3aaccd2aba85bbff987e2253cf612b7339807
Rik Meijer <rikmeijer@gmail.com>
2015-09-12 21:13:13 +0200

    keep administration of courses for a student

--- src/Course.d
+++ src/Course.d
@@ -1,7 +1,5 @@
+import Student;
+
 class Course {
-//	unittest {
-//		auto course = new Course();
-//		Student student = course.enroll(new Student());
-//		assert(student.enrolled(course));
-//	}
+	
 }
--- src/Student.d
+++ src/Student.d
@@ -2,13 +2,27 @@ import Course;
 
 class Student {
 	
+	private Course[] courses;
+	
 	bool enrolled(Course course) {
+		foreach (Course enrolledCourse; this.courses) {
+			if (enrolledCourse == course) {
+				return true;
+			}
+		}
 		return false;
+	}
+	
+	void enroll(Course course) {
+		this.courses.length++;
+		this.courses[] = course;
 	}
 	
 	unittest {
 		auto student = new Student();
 		auto course = new Course();
 		assert(student.enrolled(course) == false);
+		student.enroll(course);
+		assert(student.enrolled(course));
 	}
 }
