db47d4ca4019d854d60c543eec3580fa379236d8
Rik Meijer <rikmeijer@gmail.com>
2015-09-12 22:04:37 +0200

    course cannot start a class

--- src/Course.d
+++ src/Course.d
@@ -1,17 +1,8 @@
 module teach.Course;
 
-import teach.Student;
-import teach.Class;
 
 class Course {
 	
-	Class startClass() {
-		return new Class([]);
-	}
-	
-	unittest {
-		auto course = new Course();
-		Class courseClass = course.startClass();
-	}
+
 	
 }
