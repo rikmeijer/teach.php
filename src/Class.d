34f5e7ad619d9cf68549982d3537497d87ccff9b
Rik Meijer <rikmeijer@gmail.com>
2015-09-12 22:14:38 +0200

    a class does not expect anything

--- src/Class.d
+++ src/Class.d
@@ -13,18 +13,10 @@ class Class {
 		this.students = students;
 	}
 	
-	bool expects(Student student) {
-		return student.enrolled(this.course);
-	}
-	
 	unittest {
 		auto course = new Course();
 		auto studentA = new Student();
-		studentA.enroll(course);
-		auto studentB = new Student();
 		auto studentClass = new Class(course, [studentA]);
-		assert(studentClass.expects(studentB) == false);
-		assert(studentClass.expects(studentA));
 	}
 	
 }
