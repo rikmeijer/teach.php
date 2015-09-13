2df3c481e99e97291e4eca4f4592c81921624720
Rik Meijer <rikmeijer@gmail.com>
2015-09-13 19:34:25 +0200

    start communication with user through command-line

--- src/app.d
+++ src/app.d
@@ -1,5 +1,8 @@
 import std.stdio;
+import std.string;
 
 void main() {
-	writeln("Hello World.");
+	write("Hello, what course are the students following today? ");
+	string answer = readln();
+	write(answer);
 }
