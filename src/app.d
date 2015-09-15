51704d9cb9a5896ae3e6c317015fa593fb7aa55a
Rik Meijer <rikmeijer@gmail.com>
2015-09-15 15:22:00 +0200

    retrieve data from sqlite database

--- src/app.d
+++ src/app.d
@@ -1,21 +1,35 @@
 import std.stdio;
 import std.string;
+import std.conv;
+import d2sqlite3;
 import teach.leergang;
 
 int main() {
-	write("Hallo, voor welke leergang hebben de studenten les? ");
-	string answer = strip(readln());
-	switch (answer) {
-		case "PROG1":
-			break;
-		case "DATAB1":
-			break;
-		default:
-			writeln("Onbekende leergang '" ~ answer ~ "'...");
-			return 1;
-			
+	Database db = Database("/home/rik/teach.sqlite3");
+	
+	auto leergangen = db.execute("SELECT naam FROM leergang");
+	if (leergangen.empty()) {
+		writeln("Geen leergangen beschikbaar");
+		return 1;
 	}
-	Leergang leergang = new Leergang(answer);
+	writeln("Hallo, de volgende leergangen zijn beschikbaar: ");
+	string[string] choices;
+	foreach (leergang; leergangen) {
+		string choiceKey = to!string(choices.length);
+		string choice = leergang["naam"].as!string;
+		
+		writeln(choiceKey ~ ". " ~ choice);
+		choices[choiceKey] = choice;
+	}
+	write("welke leergang wil je gebruiken? ");
+	
+	string answer = strip(readln());
+	if (answer !in choices) {
+		writeln("Onbekende leergang '" ~ answer ~ "'...");
+		return 1;
+	}
+	
+	Leergang leergang = new Leergang(choices[answer]);
 			
 	return 0;
 }
