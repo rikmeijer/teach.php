53549852aecd2bab77261f1a61df00bca273aafb
Rik Meijer <rikmeijer@gmail.com>
2015-09-16 16:39:03 +0200

    remove verzuim, not needed yet

--- src/student.d
+++ src/student.d
@@ -1,19 +1,5 @@
 module teach.student;
 
-import teach.leergang;
-import teach.contactmoment;
-
 class Student {
-	
-	
-	public void verzuim(Contactmoment contactmoment) {
-		
-	}
-	
-	unittest {
-		auto student = new Student();
-		auto leergang = new Leergang("PROG1");
-		Contactmoment contactmoment = new Contactmoment(leergang);
-		student.verzuim(contactmoment);
-	}
+
 }
