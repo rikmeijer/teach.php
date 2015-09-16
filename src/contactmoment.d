42ceb420286a93e666393a191dc942fded9f9363
Rik Meijer <rikmeijer@gmail.com>
2015-09-16 16:38:13 +0200

    use contactmoment struct

--- src/contactmoment.d
+++ src/contactmoment.d
@@ -5,15 +5,23 @@ import teach.leergang;
 
 struct contactmoment {
 	
+	string naam;
+	string leergang_id;
+	
 	public prepared_query prepareInsert() {
-		return prepared_query("INSERT INTO contactmoment (`naam`, `leergang_id`) VALUES (?, ?)");
+		return prepared_query("INSERT INTO contactmoment (`naam`, `leergang_id`) VALUES (?, ?)", [
+			this.naam,
+			this.leergang_id
+			]);
 	}
 }
 
 unittest {
-	contactmoment cm = contactmoment();
+	contactmoment cm = contactmoment("TEST", "1");
 	prepared_query pq = cm.prepareInsert();
 	assert(pq.query == "INSERT INTO contactmoment (`naam`, `leergang_id`) VALUES (?, ?)");
+	assert(pq.parameters[0] == "TEST");
+	assert(pq.parameters[1] == "1");
 }
 
 class Contactmoment {
--- src/sql/sql.d
+++ src/sql/sql.d
@@ -2,5 +2,5 @@ module teach.sql;
 
 struct prepared_query {
 	string query;
-	string[string] parameters;
+	string[] parameters;
 }
