1e939de5fa7ea5d1bdfacd7b522eb6dd7d5580f7
Rik Meijer <rikmeijer@gmail.com>
2015-09-30 10:02:08 +0200

    create sql Table representation

---
+++ src/sql/table.d
@@ -0,0 +1,24 @@
+module sql.table;
+
+import sql.select;
+
+class Table {
+	
+	string identifier;
+	
+	this(string identifier) {
+		this.identifier = identifier;
+	}
+	
+	public Select select(string[] fields) {
+		return new Select(fields, this.identifier);
+	}
+	
+	unittest {
+		import dunit.toolkit;
+		
+		Table studentgroep = new Table("studentgroep");
+		Select studentgroepQuery = studentgroep.select(["id", "naam"]);
+		assertEqual("SELECT id, naam FROM studentgroep", studentgroepQuery.prepare().query);
+	}
+}
