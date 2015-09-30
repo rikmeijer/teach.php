5c2dd6fb57b76ffbafcfa13e58b79fc0aa9a10b0
Rik Meijer <rikmeijer@gmail.com>
2015-09-30 13:11:15 +0200

    wrap schema around sqlitedatabase

--- src/sql/schema.d
+++ src/sql/schema.d
@@ -2,7 +2,20 @@ module sql.schema;
 
 import sql.table;
 
+import d2sqlite3;
+
 class Schema {
+	private Database sqlite_database;
+	
+	public this() {
+	}
+	
+	/**
+	 * SQLite constructor
+	 */
+	public this(Database database) {
+		this.sqlite_database = database;
+	}
 	
 	public Table table(string tableIdentifier) {
 		return new Table(tableIdentifier);
@@ -12,6 +25,18 @@ class Schema {
 		import dunit.toolkit;
 		
 		Schema teach = new Schema();
+		Table studentgroep = teach.table("studentgroep");
+	}
+	
+	static sqlite(string path) {
+		return new Schema(Database(path));
+	}
+	
+	unittest {
+		import dunit.toolkit;
+		
+		Schema teach = Schema.sqlite("/tmp/database.sqlite3");
+		assertEqual("/tmp/database.sqlite3", teach.sqlite_database.attachedFilePath());
 		Table studentgroep = teach.table("studentgroep");
 	}
 }
