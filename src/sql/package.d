a92826c69de1f26f24052ce910ba7e97b0466418
Rik Meijer <rikmeijer@gmail.com>
2015-10-14 13:06:33 +0200

    start converting to PHP

--- src/sql/package.d
+++
@@ -1,7 +0,0 @@
-module sql;
-
-public import sql.query;
-public import sql.schema;
-public import sql.table;
-public import sql.select;
-public import sql.condition;
--- src/student.d
+++
@@ -1,5 +0,0 @@
-module teach.student;
-
-class Student {
-
-}
