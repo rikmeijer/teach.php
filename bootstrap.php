90835ddce4dcb035d771125243513d9b21ab003f
Rik Meijer <rikmeijer@gmail.com>
2015-10-21 22:32:14 +0200

    remove unused constants

--- bootstrap.php
+++ bootstrap.php
@@ -5,13 +5,4 @@
  */
 return function() {
 	require __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
-
-	define("MI_VERBAAL_LINGUISTISCH", "verbaal-linguistisch");
-	define("MI_LOGISCH_MATHEMATISCH", "logisch-mathematisch");
-	define("MI_VISUEEL_RUIMTELIJK", "visueel-ruimtelijk");
-	define("MI_MUZIKAAL_RITMISCH", "muzikaal-ritmisch");
-	define("MI_LICHAMELIJK_KINESTHETISCH", "lichamelijk-kinesthetisch");
-	define("MI_NATURALISTISCH", "naturalistisch");
-	define("MI_INTERPERSOONLIJK", "interpersoonlijk");
-	define("MI_INTRAPERSOONLIJK", "intrapersoonlijk");
 };
