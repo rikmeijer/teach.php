a4533fd2e0f027c05a7bd78dca0367d9b4ce24cf
Rik Meijer <rikmeijer@gmail.com>
2015-10-21 20:46:04 +0200

    change createElement signature

--- src/adapters/HTML/Factory.php
+++ src/adapters/HTML/Factory.php
@@ -3,16 +3,17 @@ namespace Teach\Adapters\HTML;
 
 final class Factory
 {
+
     /**
-     * 
-     * @param array $definition
+     *
+     * @param array $definition            
      * @return Element
      */
-    public function createElement(array $definition)
+    public function createElement($tagName, array $attributes, array $elements)
     {
-        foreach ($definition as $elementIdentifier => $elementDefinition) {
-            $element = new Element($elementIdentifier);
-            $element->append(new Text($elementDefinition[0]));
+        $element = new Element($tagName);
+        foreach ($elements as $elementDefinition) {
+            $element->append(new Text($elementDefinition));
         }
         return $element;
     }
--- tests/src/adapters/HTML/FactoryTest.php
+++ tests/src/adapters/HTML/FactoryTest.php
@@ -6,7 +6,9 @@ class FactoryTest extends \PHPUnit_Framework_TestCase
     public function testCreateElement()
     {
         $object = new Factory();
-        $html = $object->createElement(['a' => ['Hello World']])->render();
+        $html = $object->createElement('a', [], ['Hello World'])->render();
         $this->assertEquals('<a>Hello World</a>', $html);
     }
+    
+    
 }
