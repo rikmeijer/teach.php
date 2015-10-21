99430be2765ec7e786f7d680a1a66b9e59123fb8
Rik Meijer <rikmeijer@gmail.com>
2015-10-21 20:22:48 +0200

    support attributes

--- src/adapters/HTML/Element.php
+++ src/adapters/HTML/Element.php
@@ -4,14 +4,25 @@ namespace Teach\Adapters\HTML;
 final class Element
 {
     private $tagName;
+    private $attributes = array();
     
     public function __construct($tagName)
     {
         $this->tagName = $tagName;
     }
     
+    public function attribute($attributeIdentifier, $attributeValue)
+    {
+        $this->attributes[] = $attributeIdentifier . '="' . $attributeValue . '"';
+    }
+    
     public function render()
     {
-        return '<' . $this->tagName . '></' . $this->tagName . '>';
+        if (count($this->attributes) === 0) {
+            $attributeHTML = '';
+        } else {
+            $attributeHTML = ' ' . join(' ', $this->attributes);
+        }
+        return '<' . $this->tagName . $attributeHTML . '></' . $this->tagName . '>';
     }
 }
--- tests/src/adapters/HTML/ElementTest.php
+++ tests/src/adapters/HTML/ElementTest.php
@@ -9,4 +9,12 @@ class ElementTest extends \PHPUnit_Framework_TestCase
         $html = $object->render();
         $this->assertEquals('<table></table>', $html);
     }
+
+    public function testRenderAttributes()
+    {
+        $object = new Element("table");
+        $object->attribute("class", "twin-cell");
+        $html = $object->render();
+        $this->assertEquals('<table class="twin-cell"></table>', $html);
+    }
 }
