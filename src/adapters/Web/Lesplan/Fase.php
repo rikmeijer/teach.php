f34b75a71204cee059169a249f0acfd3dc47e7e4
Rik Meijer <rikmeijer@gmail.com>
2015-10-21 21:47:02 +0200

    convert Fase to adhere to new HTML-prototype makeup

--- src/adapters/Web/Lesplan/Fase.php
+++ src/adapters/Web/Lesplan/Fase.php
@@ -3,25 +3,42 @@ namespace Teach\Adapters\Web\Lesplan;
 
 final class Fase
 {
+
     private $caption;
-    
+
     public function __construct($caption)
     {
         $this->caption = $caption;
     }
 
     /**
+     *
      * @return array
      */
     public function generateFirstStep()
     {
-        return ['table' => [['class' => 'two-columns'], [
-            'caption' => $this->caption,
-            'tr' => [[], [
-                
-                'th' => 'werkvorm'
-                
-            ]]
-        ]]];
+        return [
+            [
+                'table',
+                [
+                    'class' => 'two-columns'
+                ],
+                [
+                    [
+                        'caption',
+                        $this->caption
+                    ],
+                    [
+                        'tr',
+                        [],
+                        [
+                            
+                            ['th', 'werkvorm']
+                        ]
+                    ]
+                ]
+            ]
+            
+        ];
     }
 }
--- tests/src/adapters/Web/Lesplan/FaseTest.php
+++ tests/src/adapters/Web/Lesplan/FaseTest.php
@@ -7,9 +7,13 @@ class FaseTest extends \PHPUnit_Framework_TestCase
     {
         $object = new Fase("Beautiful caption");
         $html = $object->generateFirstStep();
-        $this->assertEquals('two-columns', $html['table'][0]['class']);
-        $this->assertEquals("Beautiful caption", $html['table'][1]['caption']);
+        $this->assertEquals('table', $html[0][0]);
+        $this->assertEquals('two-columns', $html[0][1]['class']);
+        $this->assertEquals("caption", $html[0][2][0][0]);
+        $this->assertEquals("Beautiful caption", $html[0][2][0][1]);
 
-        $this->assertEquals("werkvorm", $html['table'][1]['tr'][1]['th']);
+        $this->assertEquals("tr", $html[0][2][1][0]);
+        $this->assertEquals("th", $html[0][2][1][2][0][0]);
+        $this->assertEquals("werkvorm", $html[0][2][1][2][0][1]);
     }
 }
