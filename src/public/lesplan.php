a011772f8e476c43d02b97f5825218b26961308c
Rik Meijer <rikmeijer@gmail.com>
2015-10-22 09:34:53 +0200

    use shorthand array ([])

--- src/public/lesplan.php
+++ src/public/lesplan.php
@@ -2,175 +2,175 @@
 $applicationBootstrap = require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap.php';
 $applicationBootstrap();
 
-$lesplan = array(
-    'opleiding' => 'HBO-informatica (voltijd)', // <!--  del>deeltijd</del>/-->
+$lesplan = [
+    'opleiding' => 'HBO-informatica (voltijd]', // <!--  del>deeltijd</del>/-->
     'vak' => 'Programmeren 1',
     'les' => 'Blok 1 / Week 1 / Les 1',
-    'Beginsituatie' => array(
-        'doelgroep' => array(
+    'Beginsituatie' => [
+        'doelgroep' => [
             'beschrijving' => 'eerstejaars HBO-studenten',
             'ervaring' => 'geen', // <!--  del>veel</del>, <del>redelijk veel</del>, <del>weinig</del>, -->geen
             'grootte' => '16 personen'
-        ),
+        ],
         'starttijd' => '08:45',
         'eindtijd' => '10:20',
         'duur' => '95 minuten',
         'ruimte' => 'beschikking over vaste computers',
         'overige' => 'nvt',
-        'media' => array(
+        'media' => [
             'filmfragment matrix',
             'rode en groene briefjes/post-its voor feedback',
             'presentatie',
             'voorbeeldproject voor aanvullende feedback',
-        )
-    ),
-    'Introductie' => array(
-        "Activerende opening" => array(
+        ]
+    ],
+    'Introductie' => [
+        "Activerende opening" => [
             'inhoud' => 'Scené uit de matrix tonen waarop wordt gezegd: "I don\'t even see the code". Wie kent deze film? Een ervaren programmeur zal een vergelijkbaar gevoel hebben bij code: programmeren is een visualisatie kunnen uitdrukken in code en vice versa.',
             'werkvorm' => "film",
             'organisatievorm' => "plenair",
             'werkvormsoort' => "ijsbreker",
             'tijd' => "5",
-            'intelligenties' => array(
+            'intelligenties' => [
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_VISUEEL_RUIMTELIJK,
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK,
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTRAPERSOONLIJK
-            )
-        ),
-        "Focus" => array(
+            ]
+        ],
+        "Focus" => [
             "inhoud" => "Visie, Leerdoelen, Programma, Afspraken",
             "werkvorm" => "presentatie",
             "organisatievorm" => "plenair",
             "werkvormsoort" => "docent gecentreerd",
             "tijd" => "4",
-            "intelligenties" => array(
+            "intelligenties" => [
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_LOGISCH_MATHEMATISCH,
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-            )
-        ),
-        "Voorstellen" => array(
+            ]
+        ],
+        "Voorstellen" => [
             "inhoud" => "Voorstellen Docent",
             "werkvorm" => "presentatie",
             "organisatievorm" => "plenair",
             "werkvormsoort" => "docent gecentreerd",
             "tijd" => "4",
-            "intelligenties" => array(
+            "intelligenties" => [
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_LOGISCH_MATHEMATISCH,
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-            )
-        )
-    ),
-    'Kern' => array(
-        "Zelfstandig eclipse installeren" => array(
-            "Ervaren" => array(
+            ]
+        ]
+    ],
+    'Kern' => [
+        "Zelfstandig eclipse installeren" => [
+            "Ervaren" => [
                 "inhoud" => "Tonen afbeeldingen van werkomgevingen: wie herkent de werkomgeving?",
                 "werkvorm" => "verhalen vertellen bij foto's",
                 "organisatievorm" => "plenair",
                 "werkvormsoort" => "ijsbreker",
                 "tijd" => "5",
-                "intelligenties" => array(
+                "intelligenties" => [
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_VISUEEL_RUIMTELIJK,
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_NATURALISTISCH,
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                )
-            ),
-            "Reflecteren" => array(
+                ]
+            ],
+            "Reflecteren" => [
                 "inhoud" => "•	Link leggen naar een programmeeromgeving: niet fysiek, maar virtueel.
 •	Wie kan bedenken wat voor gereedschap erbij programmeren komt kijken?",
                 "werkvorm" => "brainstormen",
                 "organisatievorm" => "plenair",
                 "werkvormsoort" => "discussie",
                 "tijd" => "5",
-                "intelligenties" => array(
+                "intelligenties" => [
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTRAPERSOONLIJK,
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                )
-            ),
-            "Conceptualiseren" => array(
-                "inhoud" => "•	Kort uitleggen wat IDE/eclipse is (programmeeromgeving)/waarvoor het wordt gebruikt. 
+                ]
+            ],
+            "Conceptualiseren" => [
+                "inhoud" => "•	Kort uitleggen wat IDE/eclipse is (programmeeromgeving]/waarvoor het wordt gebruikt. 
 •	Korte demo ter kennismaking
 •	Wat zijn de randvoorwaarden van de installatie?",
-                "werkvorm" => "presentatie (visueel ondersteunen, laatste sheet met randvoorwaarden open laten)",
+                "werkvorm" => "presentatie (visueel ondersteunen, laatste sheet met randvoorwaarden open laten]",
                 "organisatievorm" => "plenair",
                 "werkvormsoort" => "docentgecentreerd",
                 "tijd" => "5",
-                "intelligenties" => array(
+                "intelligenties" => [
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_VISUEEL_RUIMTELIJK,
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                )
-            ),
-            "Toepassen" => array(
+                ]
+            ],
+            "Toepassen" => [
                 "inhoud" => "•	Student installeert zelf eclipse
-•	Aanvullende opdracht (capaciteit): importeren voorbeeldproject van blackboard of een nieuw project aanmaken
+•	Aanvullende opdracht (capaciteit]: importeren voorbeeldproject van blackboard of een nieuw project aanmaken
 •	Na 10min controleren of dit bij iedereen is gelukt",
                 "werkvorm" => "verwerkingsopdracht",
                 "organisatievorm" => "plenair",
                 "werkvormsoort" => "individuele werkopdracht",
                 "tijd" => "15",
-                "intelligenties" => array(
+                "intelligenties" => [
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTRAPERSOONLIJK
-                )
-            )
-        ),
-        "Java-code lezen en uitleggen wat er gebeurt" => array(
-            "Ervaren" => array(
+                ]
+            ]
+        ],
+        "Java-code lezen en uitleggen wat er gebeurt" => [
+            "Ervaren" => [
                 "inhoud" => "Achterhalen wie wel eens adhv van een recept/handleiding heeft gewerkt.",
                 "werkvorm" => "metafoor",
                 "organisatievorm" => "plenair",
                 "werkvormsoort" => "ijsbreker",
                 "tijd" => "5",
-                "intelligenties" => array(
+                "intelligenties" => [
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                )
-            ),
-            "Reflecteren" => array(
+                ]
+            ],
+            "Reflecteren" => [
                 "inhoud" => "Studenten met buurman/vrouw overleggen hoeveel verschillende stappen er zijn bij het uitvoeren van een handleiding. Tijdens het uitvoeren van taken voeren wij onbewust veel contextgevoelige taken uit een computer kent dit niet.",
                 "werkvorm" => "brainstormen",
                 "organisatievorm" => "groepswerk",
                 "werkvormsoort" => "discussie",
                 "tijd" => "5",
-                "intelligenties" => array(
+                "intelligenties" => [
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_LOGISCH_MATHEMATISCH,
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTRAPERSOONLIJK,
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                )
-            ),
-            "Conceptualiseren" => array(
-                "inhoud" => "Tonen pseudo-code bij vorig recept of handleiding (bijv. IKEA handleiding)",
+                ]
+            ],
+            "Conceptualiseren" => [
+                "inhoud" => "Tonen pseudo-code bij vorig recept of handleiding (bijv. IKEA handleiding]",
                 "werkvorm" => "demonstratie",
                 "organisatievorm" => "plenair",
                 "werkvormsoort" => "docentgecentreerd",
                 "tijd" => "10",
-                "intelligenties" => array(
+                "intelligenties" => [
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_VISUEEL_RUIMTELIJK,
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                )
-            ),
-            "Toepassen" => array(
-                "inhoud" => "Studenten voeren SIMPEL (waarin zij moeten uitleggen wat de code doet) en BASIS (schrijven pseudo-code) opdrachten uit. Eventueel COMPLEX (zelf code schrijven) als voorschot op volgende week.",
+                ]
+            ],
+            "Toepassen" => [
+                "inhoud" => "Studenten voeren SIMPEL (waarin zij moeten uitleggen wat de code doet] en BASIS (schrijven pseudo-code] opdrachten uit. Eventueel COMPLEX (zelf code schrijven] als voorschot op volgende week.",
                 "werkvorm" => "verwerkingsopdracht",
                 "organisatievorm" => "plenair",
                 "werkvormsoort" => "individuele werkopdracht",
                 "tijd" => "30",
-                "intelligenties" => array(
+                "intelligenties" => [
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
                     \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTRAPERSOONLIJK
-                )
-            )
-        )
-    ),
-    'Afsluiting' => array(
-        "Huiswerk" => array(
+                ]
+            ]
+        ]
+    ],
+    'Afsluiting' => [
+        "Huiswerk" => [
             "inhoud" => "-	Challenge voor eerstvolgende les maken
 -	Practicum opdrachten thuis afronden
 -	Huiswerk maken als extra oefening",
@@ -178,36 +178,36 @@ $lesplan = array(
             "organisatievorm" => "plenair",
             "werkvormsoort" => "docentgecentreerd",
             "tijd" => "2",
-            "intelligenties" => array(
+            "intelligenties" => [
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-            )
-        ),
-        "Evaluatie" => array(
+            ]
+        ],
+        "Evaluatie" => [
             "inhoud" => "Verzamelen feedback papiertjes",
             "werkvorm" => "nabespreking",
             "organisatievorm" => "plenair",
             "werkvormsoort" => "ijsbreker",
             "tijd" => "3",
-            "intelligenties" => array(
+            "intelligenties" => [
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-            )
-        ),
-        "Pakkend slot" => array(
+            ]
+        ],
+        "Pakkend slot" => [
             "inhoud" => "Foto",
             "werkvorm" => "presentatie",
             "organisatievorm" => "plenair",
             "werkvormsoort" => "docentgecentreerd",
             "tijd" => "1",
-            "intelligenties" => array(
+            "intelligenties" => [
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_VISUEEL_RUIMTELIJK,
                 \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTRAPERSOONLIJK
-            )
-        )
-    )
-);
+            ]
+        ]
+    ]
+];
 
 function renderActiviteit($naam, array $werkvorm)
 {
