0649429b63f86c832161fb55ceaa87af87967343
Rik Meijer <rikmeijer@gmail.com>
2015-10-22 14:49:36 +0200

    support Activiteiten onder Thema

--- src/adapters/Web/Lesplan/Thema.php
+++ src/adapters/Web/Lesplan/Thema.php
@@ -4,10 +4,20 @@ namespace Teach\Adapters\Web\Lesplan;
 final class Thema implements \Teach\Adapters\HTML\LayoutableInterface
 {
     private $title;
-
+    
+    /**
+     * 
+     * @var Activiteit[]
+     */
+    private $activiteiten = [];
+    
     public function __construct($title)
     {
         $this->title = $title;
+    }
+    
+    public function addActiviteit(Activiteit $activiteit) {
+        $this->activiteiten[] = $activiteit;
     }
 
     /**
@@ -16,16 +26,19 @@ final class Thema implements \Teach\Adapters\HTML\LayoutableInterface
      */
     public function generateHTMLLayout()
     {
+        $activiteitenHTML = [];
+        foreach ($this->activiteiten as $activiteit) {
+            $activiteitenHTML = array_merge($activiteitenHTML, $activiteit->generateHTMLLayout());
+        }
+        
         return [
             [
                 'section',
                 [],
-                [
-                    [
-                        'h3',
-                        $this->title
-                    ]
-                ]
+                array_merge([[
+                    'h3',
+                    $this->title
+                ]], $activiteitenHTML)
             ]
             
         ];
--- tests/src/adapters/Web/Lesplan/ThemaTest.php
+++ tests/src/adapters/Web/Lesplan/ThemaTest.php
@@ -13,4 +13,36 @@ class ThemaTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals("h3", $html[0][HTMLFactory::CHILDREN][0][HTMLFactory::TAG]);
         $this->assertEquals("Thema 1: zelf de motor reviseren", $html[0][HTMLFactory::CHILDREN][0][HTMLFactory::TEXT]);
     }
+    
+
+    public function testGenerateHTMLLayoutActiviteiten()
+    {
+        $object = new Thema("Thema 1: zelf de motor reviseren");
+        
+        $object->addActiviteit(new Activiteit("Reflecteren", $werkvorm = array(
+                "inhoud" => "•	Link leggen naar een programmeeromgeving: niet fysiek, maar virtueel.
+•	Wie kan bedenken wat voor gereedschap erbij programmeren komt kijken?",
+                "werkvorm" => "brainstormen",
+                "organisatievorm" => "plenair",
+                "werkvormsoort" => "discussie",
+                "tijd" => "5",
+                "intelligenties" => array(
+                    Activiteit::MI_VERBAAL_LINGUISTISCH,
+                    Activiteit::MI_INTRAPERSOONLIJK,
+                    Activiteit::MI_INTERPERSOONLIJK
+                )
+            )));
+        $html = $object->generateHTMLLayout();
+        $this->assertEquals('section', $html[0][HTMLFactory::TAG]);
+        $this->assertEquals("h3", $html[0][HTMLFactory::CHILDREN][0][HTMLFactory::TAG]);
+        $this->assertEquals("Thema 1: zelf de motor reviseren", $html[0][HTMLFactory::CHILDREN][0][HTMLFactory::TEXT]);
+        
+        $row = $html[0][HTMLFactory::CHILDREN][1][HTMLFactory::CHILDREN][4];
+        $this->assertEquals("tr", $row[HTMLFactory::TAG]);
+        $this->assertEquals("th", $row[HTMLFactory::CHILDREN][0][HTMLFactory::TAG]);
+        $this->assertEquals("inhoud", $row[HTMLFactory::CHILDREN][0][HTMLFactory::TEXT]);
+        $this->assertEquals("td", $row[HTMLFactory::CHILDREN][1][HTMLFactory::TAG]);
+        $this->assertEquals('3', $row[HTMLFactory::CHILDREN][1][HTMLFactory::ATTRIBUTES]['colspan']);
+        $this->assertEquals($werkvorm['inhoud'], $row[HTMLFactory::CHILDREN][1][HTMLFactory::CHILDREN][0]);
+    }
 }
