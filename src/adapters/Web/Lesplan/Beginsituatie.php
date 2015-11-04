37c14425293a1e65ff904f3b77bdf725c0a2658f
Rik Meijer <rikmeijer@gmail.com>
2015-11-04 12:58:26 +0100

    start formalizing beginsituatie

---
+++ src/adapters/Web/Lesplan/Beginsituatie.php
@@ -0,0 +1,124 @@
+<?php
+namespace Teach\Adapters\Web\Lesplan;
+
+final class Beginsituatie implements \Teach\Adapters\HTML\LayoutableInterface
+{
+
+    private $beginsituatie;
+
+    public function __construct(array $beginsituatie)
+    {
+        $this->beginsituatie = $beginsituatie;
+    }
+
+    /**
+     *
+     * @return array
+     */
+    public function generateHTMLLayout()
+    {
+        
+        return [
+            ['h3', 'Beginsituatie'],
+//             [
+//                 'table',
+//                 [
+//                     'class' => 'two-columns'
+//                 ],
+//                 [
+//                     [
+//                         'caption',
+//                         $this->caption
+//                     ],
+//                     [
+//                         'tr',
+//                         [],
+//                         [
+//                             [
+//                                 'th',
+//                                 'werkvorm'
+//                             ],
+//                             [
+//                                 'td',
+//                                 $this->werkvorm['werkvorm']
+//                             ],
+//                             [
+//                                 'th',
+//                                 'organisatievorm'
+//                             ],
+//                             [
+//                                 'td',
+//                                 $this->werkvorm['organisatievorm']
+//                             ]
+//                         ]
+//                     ],
+//                     [
+//                         'tr',
+//                         [],
+//                         [
+//                             [
+//                                 'th',
+//                                 'tijd'
+//                             ],
+//                             [
+//                                 'td',
+//                                 $this->werkvorm['tijd'] . ' minuten'
+//                             ],
+//                             [
+//                                 'th',
+//                                 'soort werkvorm'
+//                             ],
+//                             [
+//                                 'td',
+//                                 $this->werkvorm['werkvormsoort']
+//                             ]
+//                         ]
+//                     ],
+//                     [
+//                         'tr',
+//                         [],
+//                         [
+//                             [
+//                                 'th',
+//                                 'intelligenties'
+//                             ],
+//                             [
+//                                 'td',
+//                                 [
+//                                     'colspan' => '3'
+//                                 ],
+//                                 [
+//                                     [
+//                                         'ul',
+//                                         [
+//                                             'class' => "meervoudige-intelligenties"
+//                                         ],
+//                                         $intelligentieListItems
+//                                     ]
+//                                 ]
+//                             ]
+//                         ]
+//                     ],
+//                     [
+//                         'tr',
+//                         [],
+//                         [
+//                             [
+//                                 'th',
+//                                 'inhoud'
+//                             ],
+//                             [
+//                                 'td',
+//                                 [
+//                                     'colspan' => '3'
+//                                 ],
+//                                 $inhoudChildren
+//                             ]
+//                         ]
+//                     ]
+//                 ]
+//             ]
+        ]
+        ;
+    }
+}
---
+++ tests/src/adapters/Web/Lesplan/BeginsituatieTest.php
@@ -0,0 +1,40 @@
+<?php
+namespace Teach\Adapters\Web\Lesplan;
+
+use \Teach\Adapters\HTML\Factory as HTMLFactory;
+
+class BeginsituatieTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testGenerateHTMLLayout()
+    {
+        $object = new Beginsituatie([
+            'doelgroep' => [
+                'beschrijving' => 'eerstejaars HBO-studenten',
+                'ervaring' => 'geen', // <!-- del>veel</del>, <del>redelijk veel</del>, <del>weinig</del>, -->geen
+                'grootte' => '16 personen'
+            ],
+            'starttijd' => '08:45',
+            'eindtijd' => '10:20',
+            'duur' => '95 minuten',
+            'ruimte' => 'beschikking over vaste computers',
+            'overige' => 'nvt',
+            'media' => [
+                'filmfragment matrix',
+                'countdown timer voor toepassingsfases (optioneel)',
+                'voorbeeld IKEA-handleiding + uitgewerkte pseudo-code',
+                'rode en groene briefjes/post-its voor feedback',
+                'presentatie',
+                'voorbeeldproject voor aanvullende feedback'
+            ]
+        ]);
+        $html = $object->generateHTMLLayout();
+        $this->assertEquals('h3', $html[0][HTMLFactory::TAG]);
+        $this->assertEquals("Beginsituatie", $html[0][HTMLFactory::TEXT]);
+        
+//         $this->assertEquals('table', $html[0][HTMLFactory::TAG]);
+//         $this->assertEquals('two-columns', $html[0][HTMLFactory::ATTRIBUTES]['class']);
+//         $this->assertEquals("caption", $html[0][HTMLFactory::CHILDREN][0][HTMLFactory::TAG]);
+//         $this->assertEquals("Reflecteren", $html[0][HTMLFactory::CHILDREN][0][HTMLFactory::TEXT]);
+    }
+}
