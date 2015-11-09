621df92cd87fe91c85107a49a7de3d62303af262
Rik Meijer <rikmeijer@gmail.com>
2015-11-09 19:37:22 +0100

    use makeTable in Beginsituatie

--- src/adapters/Web/Lesplan/Beginsituatie.php
+++ src/adapters/Web/Lesplan/Beginsituatie.php
@@ -3,8 +3,9 @@ namespace Teach\Adapters\Web\Lesplan;
 
 final class Beginsituatie implements \Teach\Adapters\HTML\LayoutableInterface
 {
+
     private $opleiding;
-    
+
     private $beginsituatie;
 
     public function __construct($opleiding, array $beginsituatie)
@@ -19,113 +20,30 @@ final class Beginsituatie implements \Teach\Adapters\HTML\LayoutableInterface
      */
     public function generateHTMLLayout(\Teach\Adapters\HTML\Factory $factory)
     {
-        
         return [
-            ['h3', 'Beginsituatie'],
             [
-                'table',
-                [],
+                'h3',
+                'Beginsituatie'
+            ],
+            $factory->makeTable(null, [
                 [
-                    [
-                        'tr',
-                        [],
-                        [
-                            [
-                                'th',
-                                'doelgroep'
-                            ],
-                            [
-                                'td',
-                                $this->beginsituatie['doelgroep']['beschrijving']
-                            ],
-                            [
-                                'th',
-                                'opleiding'
-                            ],
-                            [
-                                'td',
-                                $this->opleiding
-                            ]
-                        ]
-                    ],
-                    [
-                        'tr',
-                        [],
-                        [
-                            [
-                                'th',
-                                'ervaring'
-                            ],
-                            [
-                                'td',
-                                $this->beginsituatie['doelgroep']['ervaring']
-                            ],
-                            [
-                                'th',
-                                'groepsgrootte'
-                            ],
-                            [
-                                'td',
-                                $this->beginsituatie['doelgroep']['grootte']
-                            ]
-                        ]
-                    ],
-                    [
-                        'tr',
-                        [],
-                        [
-                            [
-                                'th',
-                                'tijd'
-                            ],
-                            [
-                                'td',
-                                [
-                                    'colspan' => '3'
-                                ],
-                                [
-                                    'van ' . $this->beginsituatie['starttijd'] . ' tot ' . $this->beginsituatie['eindtijd'] . ' (' . $this->beginsituatie['duur'] . ' minuten)'
-                                ]
-                            ]
-                        ]
-                    ],
-                    [
-                        'tr',
-                        [],
-                        [
-                            [
-                                'th',
-                                'ruimte'
-                            ],
-                            [
-                                'td',
-                                [
-                                    'colspan' => '3'
-                                ],
-                                [$this->beginsituatie['ruimte']]
-                            ]
-                        ]
-                    ],
-                    [
-                        'tr',
-                        [],
-                        [
-                            [
-                                'th',
-                                'overige'
-                            ],
-                            [
-                                'td',
-                                [
-                                    'colspan' => '3'
-                                ],
-                                [$this->beginsituatie['overige']]
-                            ]
-                        ]
-                    ]
+                    'doelgroep' => $this->beginsituatie['doelgroep']['beschrijving'],
+                    'opleiding' => $this->opleiding
+                ],
+                [
+                    'ervaring' => $this->beginsituatie['doelgroep']['ervaring'],
+                    'groepsgrootte' => $this->beginsituatie['doelgroep']['grootte']
+                ],
+                [
+                    'tijd' => 'van ' . $this->beginsituatie['starttijd'] . ' tot ' . $this->beginsituatie['eindtijd'] . ' (' . $this->beginsituatie['duur'] . ' minuten)'
+                ],
+                [
+                    'ruimte' => $this->beginsituatie['ruimte']
+                ],
+                [
+                    'overige' => $this->beginsituatie['overige']
                 ]
-            ]
-        ]
-        ;
+            ])
+        ];
     }
 }
