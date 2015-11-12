01abb6b1dd07ac87151e24498beebf92a48c5ae4
Rik Meijer <rikmeijer@gmail.com>
2015-11-12 15:24:17 +0100

    overige activiteiten naar database verplaatst

--- src/lesplannen/les1.php
+++ src/lesplannen/les1.php
@@ -23,6 +23,8 @@ return function (\mysqli $database) {
             WHERE 
                 id = " . $id . "
         ");
+
+        $activiteit['inhoud'] = explode(chr(10), $activiteit['inhoud']);
         $activiteit['intelligenties'] = explode(',', $activiteit['intelligenties']);
         return $activiteit;
     }
@@ -33,7 +35,13 @@ return function (\mysqli $database) {
             les.naam AS les,
             module.naam AS vak,
             contactmoment.starttijd AS starttijd,
-            contactmoment.eindtijd AS eindtijd
+            contactmoment.eindtijd AS eindtijd,
+            les.activerende_opening_id,
+            les.focus_id,
+            les.voorstellen_id,
+            les.huiswerk_id,
+            les.evaluatie_id,
+            les.pakkend_slot_id
         FROM contactmoment
         JOIN les ON les.id = contactmoment.les_id
         JOIN module ON module.id = les.module_id
@@ -69,190 +77,28 @@ return function (\mysqli $database) {
             'voorbeeldproject voor aanvullende feedback'
         ],
         'Introductie' => [
-            "Activerende opening" => getActiviteit($database, 1),
-            "Focus" => [
-                "inhoud" => "Visie, Leerdoelen, Programma, Afspraken",
-                "werkvorm" => "presentatie",
-                "organisatievorm" => "plenair",
-                "werkvormsoort" => "docent gecentreerd",
-                "tijd" => "3",
-                "intelligenties" => [
-                    \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
-                    \Teach\Adapters\Web\Lesplan\Activiteit::MI_LOGISCH_MATHEMATISCH,
-                    \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                ]
-            ],
-            "Voorstellen" => [
-                "inhoud" => "Voorstellen Docent",
-                "werkvorm" => "presentatie",
-                "organisatievorm" => "plenair",
-                "werkvormsoort" => "docent gecentreerd",
-                "tijd" => "2",
-                "intelligenties" => [
-                    \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
-                    \Teach\Adapters\Web\Lesplan\Activiteit::MI_LOGISCH_MATHEMATISCH,
-                    \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                ]
-            ]
+            "Activerende opening" => getActiviteit($database, $lesplan['activerende_opening_id']),
+            "Focus" => getActiviteit($database, $lesplan['focus_id']),
+            "Voorstellen" => getActiviteit($database, $lesplan['voorstellen_id']),
         ],
         'Kern' => [
             "Zelfstandig eclipse installeren" => [
-                "Ervaren" => [
-                    "inhoud" => "Tonen afbeeldingen van werkomgevingen: wie herkent de werkomgeving?",
-                    "werkvorm" => "verhalen vertellen bij foto's",
-                    "organisatievorm" => "plenair",
-                    "werkvormsoort" => "ijsbreker",
-                    "tijd" => "5",
-                    "intelligenties" => [
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_VISUEEL_RUIMTELIJK,
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_NATURALISTISCH,
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                    ]
-                ],
-                "Reflecteren" => [
-                    "inhoud" => [
-                        "5 minuten brainstormen om te reflecteren op de voorgaande afbeeldingen.",
-                        "De uiteindelijke vraag om te beantwoorden: 'Hoe zou een werkplaats voor een programmeur eruit zien?'",
-                        "Wat valt er op aan deze werkplaatsen?",
-                        "Link leggen naar een programmeeromgeving: niet fysiek, maar virtueel.",
-                        "Wie kan bedenken wat voor gereedschap erbij programmeren komt kijken?"
-                    ],
-                    "werkvorm" => "brainstormen",
-                    "organisatievorm" => "plenair",
-                    "werkvormsoort" => "discussie",
-                    "tijd" => "5",
-                    "intelligenties" => [
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTRAPERSOONLIJK,
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                    ]
-                ],
-                "Conceptualiseren" => [
-                    "inhoud" => [
-                        "Kort uitleggen wat IDE/eclipse is (programmeeromgeving]/waarvoor het wordt gebruikt.",
-                        "Korte demo ter kennismaking",
-                        "Wat zijn de randvoorwaarden van de installatie?"
-                    ],
-                    "werkvorm" => "presentatie (visueel ondersteunen, laatste sheet met randvoorwaarden open laten)",
-                    "organisatievorm" => "plenair",
-                    "werkvormsoort" => "docentgecentreerd",
-                    "tijd" => "5",
-                    "intelligenties" => [
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_VISUEEL_RUIMTELIJK,
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                    ]
-                ],
-                "Toepassen" => [
-                    "inhoud" => [
-                        "Student installeert zelf eclipse",
-                        "Aanvullende opdracht (capaciteit): importeren voorbeeldproject van blackboard of een nieuw project aanmaken",
-                        "Na 10min controleren of dit bij iedereen is gelukt"
-                    ],
-                    "werkvorm" => "verwerkingsopdracht",
-                    "organisatievorm" => "plenair",
-                    "werkvormsoort" => "individuele werkopdracht",
-                    "tijd" => "18",
-                    "intelligenties" => [
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTRAPERSOONLIJK
-                    ]
-                ]
+                "Ervaren" => getActiviteit($database, 4),
+                "Reflecteren" => getActiviteit($database, 5),
+                "Conceptualiseren" => getActiviteit($database, 6),
+                "Toepassen" => getActiviteit($database, 7)
             ],
             "Java-code lezen en uitleggen wat er gebeurt" => [
-                "Ervaren" => [
-                    "inhoud" => "Achterhalen wie wel eens adhv van een recept/handleiding heeft gewerkt.",
-                    "werkvorm" => "metafoor",
-                    "organisatievorm" => "plenair",
-                    "werkvormsoort" => "ijsbreker",
-                    "tijd" => "5",
-                    "intelligenties" => [
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                    ]
-                ],
-                "Reflecteren" => [
-                    "inhoud" => "Studenten met buurman/vrouw overleggen hoeveel verschillende stappen er zijn bij het uitvoeren van een handleiding. Tijdens het uitvoeren van taken voeren wij onbewust veel contextgevoelige taken uit een computer kent dit niet.",
-                    "werkvorm" => "brainstormen",
-                    "organisatievorm" => "groepswerk",
-                    "werkvormsoort" => "discussie",
-                    "tijd" => "5",
-                    "intelligenties" => [
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_LOGISCH_MATHEMATISCH,
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTRAPERSOONLIJK,
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                    ]
-                ],
-                "Conceptualiseren" => [
-                    "inhoud" => "Tonen pseudo-code bij vorig recept of handleiding (bijv. IKEA handleiding)",
-                    "werkvorm" => "demonstratie",
-                    "organisatievorm" => "plenair",
-                    "werkvormsoort" => "docentgecentreerd",
-                    "tijd" => "10",
-                    "intelligenties" => [
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_VISUEEL_RUIMTELIJK,
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                    ]
-                ],
-                "Toepassen" => [
-                    "inhoud" => [
-                        "SIMPEL: uitleggen wat de code doet",
-                        "BASIS: schrijven pseudo-code",
-                        "COMPLEX: zelf code schrijven, als voorschot op volgende week (extra uitdaging)."
-                    ],
-                    "werkvorm" => "verwerkingsopdracht",
-                    "organisatievorm" => "plenair",
-                    "werkvormsoort" => "individuele werkopdracht",
-                    "tijd" => "30",
-                    "intelligenties" => [
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
-                        \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTRAPERSOONLIJK
-                    ]
-                ]
+                "Ervaren" => getActiviteit($database, 8),
+                "Reflecteren" => getActiviteit($database, 9),
+                "Conceptualiseren" => getActiviteit($database, 10),
+                "Toepassen" => getActiviteit($database, 11)
             ]
         ],
         'Afsluiting' => [
-            "Huiswerk" => [
-                "inhoud" => [
-                    "Challenge voor eerstvolgende les maken",
-                    "Practicum opdrachten thuis afronden",
-                    "Huiswerk maken als extra oefening"
-                ],
-                "werkvorm" => "presentatie",
-                "organisatievorm" => "plenair",
-                "werkvormsoort" => "docentgecentreerd",
-                "tijd" => "2",
-                "intelligenties" => [
-                    \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
-                    \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                ]
-            ],
-            "Evaluatie" => [
-                "inhoud" => "Verzamelen feedback papiertjes",
-                "werkvorm" => "nabespreking",
-                "organisatievorm" => "plenair",
-                "werkvormsoort" => "ijsbreker",
-                "tijd" => "3",
-                "intelligenties" => [
-                    \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
-                    \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTERPERSOONLIJK
-                ]
-            ],
-            "Pakkend slot" => [
-                "inhoud" => "Foto; gerelateerd aan keuzes/condities. Misschien foto van blauwe/rode pil Matrix.",
-                "werkvorm" => "presentatie",
-                "organisatievorm" => "plenair",
-                "werkvormsoort" => "docentgecentreerd",
-                "tijd" => "1",
-                "intelligenties" => [
-                    \Teach\Adapters\Web\Lesplan\Activiteit::MI_VERBAAL_LINGUISTISCH,
-                    \Teach\Adapters\Web\Lesplan\Activiteit::MI_VISUEEL_RUIMTELIJK,
-                    \Teach\Adapters\Web\Lesplan\Activiteit::MI_INTRAPERSOONLIJK
-                ]
-            ]
+            "Huiswerk" => getActiviteit($database, $lesplan['huiswerk_id']),
+            "Evaluatie" => getActiviteit($database, $lesplan['evaluatie_id']),
+            "Pakkend slot" => getActiviteit($database, $lesplan['pakkend_slot_id'])
         ]
     ];
 };
