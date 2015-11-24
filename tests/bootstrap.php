53863b9e5f934ab604ae68f8d223858780723da6
Rik Meijer <rikmeijer@gmail.com>
2015-11-24 21:09:03 +0100

    fix fatal

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'EnitiesTest.php'
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'EnitiesTest.php';
 
 /* 
  * test specific bootstrapper
