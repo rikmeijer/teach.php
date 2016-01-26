ff6389dca122a2af28609a841e742367db57b081
Rik Meijer <rikmeijer@gmail.com>
2016-01-26 11:34:26 +0100

    fix typo

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'EnitiesTest.php';
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'EntitiesTest.php';
 
 /* 
  * test specific bootstrapper
