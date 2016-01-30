00e4b3cefd87e159f03d4a043c0e2d99a7872a2a
Rik Meijer <rikmeijer@gmail.com>
2016-01-30 17:12:06 +0100

    EntitiesTest requires dbunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'EntitiesTest.php';
-
 /* 
  * test specific bootstrapper
  */
 $applicationBootstrap = require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap.php';
 $applicationBootstrap();
+
+require_once __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'EntitiesTest.php';
