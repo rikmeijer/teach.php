c0b17377637e697a07eb1ce149acd59e0af78d50
Rik Meijer <rikmeijer@gmail.com>
2016-02-17 11:21:35 +0100

    copy pageHeader to lesplan template

--- src/public/templates/lesplan.php
+++ src/public/templates/lesplan.php
@@ -1,10 +1,16 @@
 <?php
 return function(\Teach\Adapters\HTML\Factory $factory, \Teach\Interactors\Web\Lesplan $lesplan) {
+    $variables = $lesplan->provideTemplateVariables([
+        "lesplanIdentifier",
+        "opleidingIdentifier"
+    ]);
+    
     $lines = [];
     $lines[] = "<!DOCTYPE html>";
     $lines[] = "<html>";
     $lines[] = $factory->renderTemplate(__DIR__ . DIRECTORY_SEPARATOR . "head.php");
     $lines[] = "<body>";
+    $lines[] = $factory->makeHTML([$factory->makePageHeader($factory->makeHeader1($variables["lesplanIdentifier"]), $factory->makeHeader2($variables["opleidingIdentifier"]))]);
     $lines[] = $factory->makeHTML($lesplan->generateLayout($factory));
     $lines[] = "</body>";
     $lines[] = "</html>";
