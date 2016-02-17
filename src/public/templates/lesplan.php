ddf53563609168d1adf0520621c4f9ce5c31d352
Rik Meijer <rikmeijer@gmail.com>
2016-02-17 11:43:24 +0100

    copy introductie, kern and afsluiting to lesplan template

--- src/public/templates/lesplan.php
+++ src/public/templates/lesplan.php
@@ -4,7 +4,10 @@ return function(\Teach\Adapters\HTML\Factory $factory, \Teach\Interactors\Web\Le
         "title",
         "subtitle",
         "contactmomentTitle",
-        "contactmoment"
+        "contactmoment",
+        "introductie",
+        "kern",
+        "afsluiting",
     ]);
     
     $lines = [];
@@ -14,7 +17,11 @@ return function(\Teach\Adapters\HTML\Factory $factory, \Teach\Interactors\Web\Le
     $lines[] = "<body>";
     $lines[] = $factory->renderTemplate(__DIR__ . DIRECTORY_SEPARATOR . "header.php", $variables["title"], $variables["subtitle"]);
     $lines[] = $factory->renderTemplate(__DIR__ . DIRECTORY_SEPARATOR . "lesplan" . DIRECTORY_SEPARATOR .  "contactmoment.php", $variables["contactmomentTitle"], $variables["contactmoment"]);
-    $lines[] = $factory->makeHTML($lesplan->generateLayout($factory));
+    $lines[] = $factory->makeHTML(array_merge(
+        $variables["introductie"]->generateLayout ($factory),
+        $variables["kern"]->generateLayout ($factory),
+        $variables["afsluiting"]->generateLayout ($factory)
+    ));
     $lines[] = "</body>";
     $lines[] = "</html>";
     return join(PHP_EOL, $lines);
