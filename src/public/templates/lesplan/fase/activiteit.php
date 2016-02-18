a6669a56a87c08123ac6a4474049082bccdcbc2d
Rik Meijer <rikmeijer@gmail.com>
2016-02-18 15:01:23 +0100

    activiteit does not have to create unordered lists any longer. renderTable takes care of that

--- src/public/templates/lesplan/fase/activiteit.php
+++ src/public/templates/lesplan/fase/activiteit.php
@@ -1,29 +1,19 @@
 <?php
 return function(\Teach\Adapters\HTML\Factory $factory, $title, $inhoud, $werkvorm, $organisatievorm, $werkvormsoort, $tijd, $intelligenties) {
-    return '';
-    $inhoudChildren = [];
-    if (is_string($inhoud)) {
-        $inhoudChildren[] = $inhoud;
-    } elseif (is_array($inhoud)) {
-        $inhoudChildren[] = $factory->makeUnorderedList($inhoud);
-    }
-    
-    return [
-        $factory->makeTable($title, [
-            [
-                'werkvorm' => $werkvorm,
-                'organisatievorm' => $organisatievorm
-            ],
-            [
-                'tijd' => $tijd,
-                'soort werkvorm' => $werkvormsoort
-            ],
-            [
-                'intelligenties' => [$factory->makeUnorderedList($intelligenties)]
-            ],
-            [
-                'inhoud' => $inhoudChildren
-            ]
-        ])
-    ];
+    return $factory->renderTable($title, [
+        [
+            'werkvorm' => $werkvorm,
+            'organisatievorm' => $organisatievorm
+        ],
+        [
+            'tijd' => $tijd,
+            'soort werkvorm' => $werkvormsoort
+        ],
+        [
+            'intelligenties' => $intelligenties
+        ],
+        [
+            'inhoud' => $inhoud
+        ]
+    ]);
 }; 
