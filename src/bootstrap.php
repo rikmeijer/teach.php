7d3f29cba2502466421001ee294ddaa8ef72cb1b
Rik Meijer <rikmeijer@gmail.com>
2016-03-03 16:01:13 +0100

    cleanup interface

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,19 +3,26 @@ $environmentBootstrap = require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootst
 
 interface ApplicationBootstrap {
     public function getContactmoment($contactmomentIdentifier): \Teach\Interactors\LayoutableInterface;
+    public function getEntitiesFactory(): \Teach\Entities\Factory;
 }
 
 return new class($environmentBootstrap) implements ApplicationBootstrap {
     
-    private $pdo;
+    /**
+     * 
+     * @var \EnvironmentBootstrap
+     */
+    private $environment;
     
     public function __construct(\EnvironmentBootstrap $environmentBootstrap) {
-        $this->pdo = $environmentBootstrap->getDatabase();
+        $this->environment = $environmentBootstrap;
+    }
+    
+    public function getEntitiesFactory() {
+        return new \Teach\Entities\Factory($this->environment->getDatabase());
     }
     
     public function getContactmoment($contactmomentIdentifier): \Teach\Interactors\LayoutableInterface {
-        $factory = new \Teach\Entities\Factory($this->pdo);
-        $contactmomentEntity = $factory->createContactmoment($contactmomentIdentifier);
-        return $contactmomentEntity->createLesplan(new \Teach\Interactors\Web\Lesplan\Factory());
+        return $this->getEntitiesFactory()->createContactmoment($contactmomentIdentifier)->createLesplan(new \Teach\Interactors\Web\Lesplan\Factory());
     }
 };
