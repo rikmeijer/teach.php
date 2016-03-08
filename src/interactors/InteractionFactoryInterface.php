1223113f77e47bc9aa1824e57948b32aa53b9c85
Rik Meijer <rikmeijer@gmail.com>
2016-03-08 16:47:44 +0100

    drop InteractionFactoryInterface (not used)

--- src/interactors/InteractionFactoryInterface.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace Teach\Interactors;
-
-interface InteractionFactoryInterface
-{
-    public function createInteraction(\Teach\Interactors\Interactable $interactable): \Teach\Interactors\PresentableInterface;
-}
