3cb9cd6acd1819d2af2f8ecb87481a2f86371126
Rik Meijer <rikmeijer@gmail.com>
2016-03-08 15:45:13 +0100

    use interface and anonymous class for mocking

--- tests/src/adapters/HTMLTest.php
+++ tests/src/adapters/HTMLTest.php
@@ -5,7 +5,52 @@ class HTMLTest extends \PHPUnit_Framework_TestCase
 {
     public function testRender()
     {
-        $object = new HTML(new \Teach\Adapters\HTML\Factory(__DIR__));
+        $object = new HTML(new class implements \Teach\Adapters\AdapterInterface {
+
+            /**
+             *
+             * @param int $expectedCellCount
+             * @param array $data
+             */
+            public function makeTableRow($expectedCellCount, array $data)
+            {
+                return null;
+            }
+            
+            /**
+             *
+             * @param array $listitems
+             */
+            public function makeUnorderedList(array $listitems)
+            {
+                return null;
+            }
+            
+            /**
+             *
+             * @param string $caption
+             * @param array $rows
+             */
+            public function makeTable($caption, array $rows)
+            {
+                return null;
+            }
+            
+            /**
+             *
+             * @param string $level
+             * @param string $text
+             * @return \Teach\Adapters\HTML\Element
+             */
+            public function makeHeader(string $level, string $text)
+            {
+                return null;
+            }
+            public function makeSection()
+            {
+                return null;
+            }
+        });
         $html = $object->render(new class implements \Teach\Interactors\PresentableInterface {
             public function present(\Teach\Adapters\AdapterInterface $factory): string
             {
