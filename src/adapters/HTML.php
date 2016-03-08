2aefea8ae70c8ac81318fee60c459b651f8f670f
Rik Meijer <rikmeijer@gmail.com>
2016-03-08 15:31:38 +0100

    use HTML class to start rendering

---
+++ src/adapters/HTML.php
@@ -0,0 +1,22 @@
+<?php
+namespace Teach\Adapters;
+
+final class HTML
+{    
+    
+    /**
+     * 
+     * @var HTML\Factory
+     */
+    private $factory;
+    
+    public function __construct(HTML\Factory $factory)
+    {
+        $this->factory = $factory;
+    }
+    
+    public function render(\Teach\Interactors\PresentableInterface $presentable)
+    {
+        return '<!DOCTYPE html>' . $presentable->present($this->factory);
+    }
+}
---
+++ tests/src/adapters/HTMLTest.php
@@ -0,0 +1,19 @@
+<?php
+namespace Teach\Adapters;
+
+class HTMLTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRender()
+    {
+        $object = new HTML(new \Teach\Adapters\HTML\Factory(__DIR__));
+        $html = $object->render(new class implements \Teach\Interactors\PresentableInterface {
+            public function present(\Teach\Adapters\HTML\Factory $factory): string
+            {
+                return '<body></body>';
+            }
+        });
+        $this->assertEquals('<!DOCTYPE html><body></body>', $html);
+    }
+    
+    
+}
