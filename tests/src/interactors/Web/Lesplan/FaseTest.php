0c1fdfb93dd3a7da64cb5bdc7c3312d4fb245c05
Rik Meijer <rikmeijer@gmail.com>
2016-03-10 10:01:33 +0100

    fix FaseTest

--- tests/src/interactors/Web/Lesplan/FaseTest.php
+++ tests/src/interactors/Web/Lesplan/FaseTest.php
@@ -3,18 +3,15 @@ namespace Teach\Interactions\Web\Lesplan;
 
 class FaseTest extends \PHPUnit_Framework_TestCase
 {
-    // public function testProvideTemplateVariablesOnderdelen()
-    // {
-    // $object = new Fase("Kern");
-    
-    // $object->addOnderdeel($onderdeel = new Thema("Thema 1: zelf de motor reviseren"));
-    
-    // $variables = $object->provideTemplateVariables([
-    // "title",
-    // "onderdelen"
-    // ]);
-    
-    // $this->assertEquals("Kern", $variables["title"]);
-    // $this->assertEquals($onderdeel, $variables["onderdelen"][0]);
-    // }
+
+    public function testDocument()
+    {
+        $object = new Fase("Kern");
+        
+        $object->addOnderdeel($onderdeel = new Thema("Thema 1: zelf de motor reviseren"));
+        
+        $html = $object->document(\Test\Helper::implementDocumenter());
+        
+        $this->assertEquals('section2:Kern...section3:Thema 1: zelf de motor reviseren', $html);
+    }
 }
