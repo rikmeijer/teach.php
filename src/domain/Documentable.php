8fe523a74b2a2168b630fdcfb590e330aba22fbd
Rik Meijer <rikmeijer@gmail.com>
2016-03-14 11:08:08 +0100

    remove non-value doccomment

--- src/domain/Documentable.php
+++ src/domain/Documentable.php
@@ -3,11 +3,5 @@ namespace Teach\Domain;
 
 interface Documentable
 {
-
-    /***
-     * 
-     * @param \Teach\Interactions\Documenter $adapter
-     * @return string
-     */
     function document(\Teach\Interactions\Documenter $adapter): string;
 }
