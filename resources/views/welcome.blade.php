f4a4b2dd529c5af39bdb1ada615fe98ae926e4a7
Rik Meijer <rikmeijer@gmail.com>
2016-04-25 10:26:30 +0200

    show starttijd in contactmoment-overview

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
 @section('content')
     @foreach ($contactmomenten as $contactmoment)
     	<section>
-    	<h1>{{ $contactmoment->les->module->naam }} {{ $contactmoment->les->naam }}</h1>
+    	<h1>{{ $contactmoment->les->module->naam }} {{ $contactmoment->les->naam }} {{ date('H:i', strtotime($contactmoment->starttijd)) }}</h1>
     	<ul>
     		<li><a href="/contactmomenten/{{ $contactmoment->id }}" target="_blank">Lesplan</a></li>
     		@foreach ($ipv4Adresses as $ipv4Adress)
@@ -19,7 +19,7 @@
 	<section>
 	<ul>
 		<li><a href="/les/add" target="_blank">Les toevoegen</a></li>
-		<li><a href="/contactmomenten/add" target="_blank">Contactmoment toevoegen</a></li>    		
+		<li><a href="/contactmomenten/create" target="_blank">Contactmoment toevoegen</a></li>    		
 	</ul>
 	</section>
 @endsection
