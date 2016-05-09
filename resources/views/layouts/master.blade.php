9755d743d9143e2e0479e66d65a2dea5bde74233
Rik Meijer <rikmeijer@gmail.com>
2016-05-09 11:00:29 +0200

    font was only used in Laravel template

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,9 +3,6 @@
 <html>
     <head>
         <title>Teach - @yield('title')</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
 		<link rel="stylesheet" type="text/css" href="/css/lesplan.css">	
     </head>
     <body>
