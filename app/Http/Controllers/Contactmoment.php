25f64e6c729cd3ee7beb2dc559e7418aabf18764
hameijer <hameijer@NB150170.avans.nl>
2016-05-09 17:16:56 +0200

    add support importing Avans JSON format

--- app/Http/Controllers/Contactmoment.php
+++ app/Http/Controllers/Contactmoment.php
@@ -3,63 +3,95 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Symfony\Component\CssSelector\Exception\InternalErrorException;
 
 class Contactmoment extends Controller
 {
     
     public function importFromURL() {
-        $url = \Request::get("url");
-        
-        $icalReader = new \ICal($url);
-        
-        foreach ($icalReader->events() as $event) {
-            if (array_key_exists('SUMMARY', $event) === false) {
-                continue;
-            } elseif (preg_match('/(?<module>[A-Z]+\d{1,2})/', $event['SUMMARY'], $matches) !== 1) {
-                continue;
-            }
-           
-            $module = \App\Module::where('naam', $matches['module'])->first();
-            if ($module === null) {
-                continue;
-            }
-            
-            $starttijd = (new \DateTime($event['DTSTART']));
-            $starttijd->setTimezone(new \DateTimeZone(ini_get('date.timezone')));
-            $eindtijd = (new \DateTime($event['DTEND']));
-            $eindtijd->setTimezone(new \DateTimeZone(ini_get('date.timezone')));
-            
-            $contactmoment = \App\Contactmoment::where('ical_uid', $event['UID'])->first();
-            if ($contactmoment === null) {
-                $contactmoment = \App\Contactmoment::firstOrNew(['starttijd' => $starttijd]);
-            }
-
-            $contactmoment->ical_uid = $event['UID'];
-            $contactmoment->starttijd = $starttijd;
-            $contactmoment->eindtijd = $eindtijd;
-            $contactmoment->ruimte = $event['LOCATION'];
-                        
-            if ($contactmoment->les === null) {
-                // try to find lesplan
-                $lesplan = $module->lessen()->firstOrNew([
-                    'jaar' => $contactmoment->starttijd->format('Y'),
-                    'kalenderweek' => $contactmoment->starttijd->format('W')
-                ]);
-                $lesplan->doelgroep()->associate(\App\Doelgroep::find(1));
-                $lesplan->save();
+        switch (\Request::get("type")) {
+            case "ics":
+                $url = \Request::get("url");
+                $icalReader = new \ICal($url);
                 
-                $contactmoment->les()->associate($lesplan);
-            }
-            
-            $contactmoment->save();
+                foreach ($icalReader->events() as $event) {
+                    if (array_key_exists('SUMMARY', $event) === false) {
+                        continue;
+                    } 
+                    
+                    $module = $this->extractModule($event['SUMMARY']);
+                    if ($module === null) {
+                        continue;
+                    }
+                    
+                    $this->importEvent($module, new \DateTime($event['DTSTART']), new \DateTime($event['DTEND']), $event['UID'], $event['LOCATION']);
+                }
+                
+                // remove future, imported contactmomenten which where not touched in this batch (today)
+                \App\Contactmoment::where(function($query) {
+                    $query->where('updated_at', '<', date('Y-m-d'))->orWhereNull('updated_at');
+                })->where('starttijd', '>', date('Y-m-d'))->whereNotNull('ical_uid')->delete();
+                break;
+                
+            case 'avansroosterjson':
+                foreach (json_decode(\Request::get("json"), true) as $event) {
+                    $module = $this->extractModule($event['vak']);
+                    if ($module === null) {
+                        continue;
+                    } elseif (preg_match('/in\s+\<a[^\>]+\><span[^\>]+\>(?<ruimte>\w+)/', $event['title'], $matches) !== 1) {
+                        continue;
+                    }
+                    
+                    $ruimte = $matches['ruimte'];
+                    $uid = 'Ical' . $event['start'] . $event['end'] . $ruimte . $event['vak'] . $event['param'] . '@rooster.avans.nl';
+                    $uid = str_replace('-', '', $uid);
+                    $uid = str_replace(' ', '', $uid);
+                    $this->importEvent($module, new \DateTime($event['start']), new \DateTime($event['end']), $uid, $ruimte);
+                }   
+                break;
+                
+            default:
+                return abort(500, 'Unsupported import type');
+        }
+        return view("contactmoment.imported");
+    }
+    
+    private function extractModule(string $summary)
+    {
+        if (preg_match('/(?<module>[A-Z]+\d{1,2})/', $summary, $matches) !== 1) {
+            return null;
+        }
+        return \App\Module::where('naam', $matches['module'])->first();
+    }
+    
+    private function importEvent(\App\Module $module, \DateTime $starttijd, \DateTime $eindtijd, string $uid, string $ruimte) 
+    {
+        $starttijd->setTimezone(new \DateTimeZone(ini_get('date.timezone')));
+        $eindtijd->setTimezone(new \DateTimeZone(ini_get('date.timezone')));
+        
+        $contactmoment = \App\Contactmoment::where('ical_uid', $uid)->first();
+        if ($contactmoment === null) {
+            $contactmoment = \App\Contactmoment::firstOrNew(['starttijd' => $starttijd]);
         }
         
-        // remove future, imported contactmomenten which where not touched in this batch (today)
-        \App\Contactmoment::where(function($query) {
-            $query->where('updated_at', '<', date('Y-m-d'))->orWhereNull('updated_at');
-        })->where('starttijd', '>', date('Y-m-d'))->whereNotNull('ical_uid')->delete();
+        $contactmoment->ical_uid = $uid;
+        $contactmoment->starttijd = $starttijd;
+        $contactmoment->eindtijd = $eindtijd;
+        $contactmoment->ruimte = $ruimte;
         
-        return view("contactmoment.imported");
+        if ($contactmoment->les === null) {
+            // try to find lesplan
+            $lesplan = $module->lessen()->firstOrNew([
+                'jaar' => $contactmoment->starttijd->format('Y'),
+                'kalenderweek' => (int)$contactmoment->starttijd->format('W')
+            ]);
+            $lesplan->doelgroep()->associate(\App\Doelgroep::find(1));
+            $lesplan->save();
+        
+            $contactmoment->les()->associate($lesplan);
+        }
+        
+        $contactmoment->save();
     }
     
 }
--- resources/views/contactmoment/import.blade.php
+++ resources/views/contactmoment/import.blade.php
@@ -10,7 +10,16 @@
     <section>
     	<form method="post">
     		{{ csrf_field() }}
-    		URL: <input type="text" name="url" /><br />
+    		Type: <ul>
+    		<li>
+    			<input type="radio" name="type" value="ics" selected />ICS<br/>
+    			URL: <input type="text" name="url" />
+    		</li>
+    		<li>
+    			<input type="radio" name="type" value="avansroosterjson" />Avans Rooster JSON<br/ >
+    			JSON: <textarea type="text" name="json" rows="10" cols="50"></textarea>
+    		</li>
+    		</ul> 
     		<input type="submit" value="Importeren" />
     	</form>
     </section>
