cf408dc5b517be964845edaa917ba7b4fee6d85f
Rik Meijer <rikmeijer@gmail.com>
2016-05-10 13:37:51 +0200

    move lesplan logic to Contactmoment-controller

--- app/Http/Controllers/Contactmoment.php
+++ app/Http/Controllers/Contactmoment.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -7,8 +6,52 @@ use Symfony\Component\CssSelector\Exception\InternalErrorException;
 
 class Contactmoment extends Controller
 {
-    
-    public function importFromURL() {
+
+    public function read(\App\Contactmoment $contactmoment)
+    {
+        // $code = request('code');
+        // $googleService = \OAuth::consumer('Google');
+        // if ($code === null) {
+        // return redirect((string) $googleService->getAuthorizationUri() . "&hd=avans.nl");
+        // }
+        // $token = $googleService->requestAccessToken($code);
+        // $result = json_decode($googleService->request('https://www.googleapis.com/oauth2/v1/userinfo'), true);
+        $result = \Request::old('result');
+        if ($result !== null) {
+            switch ($result[0]) {
+                case 'created':
+                    $path = explode('.', $result[1]['referencing_property']);
+                    if (substr_compare($path[0], 'thema', 0) === 0) {
+                        list ($themaQualifier, $referencedIndex) = explode('#', $path[0]);
+                        foreach ($contactmoment->les->themas as $index => $thema) {
+                            if ($index === (int) $referencedIndex) {
+                                $thema->{$path[1]} = $result[1]['value'];
+                                $thema->save();
+                            }
+                        }
+                    } elseif ($path[0] === 'les') { // expect les
+                        $contactmoment->les->{$path[1]} = $result[1]['value'];
+                        $contactmoment->les->save();
+                    } else {
+                        return abort(400);
+                    }
+                    break;
+                
+                case 'updated':
+                    break;
+                
+                default:
+                    return abort(500, 'Unknown result ' . $result[0]);
+            }
+        }
+        
+        return view('lesplan', [
+            'contactmoment' => $contactmoment
+        ]);
+    }
+
+    public function importFromURL()
+    {
         switch (\Request::get("type")) {
             case "ics":
                 $url = \Request::get("url");
@@ -17,7 +60,7 @@ class Contactmoment extends Controller
                 foreach ($icalReader->events() as $event) {
                     if (array_key_exists('SUMMARY', $event) === false) {
                         continue;
-                    } 
+                    }
                     
                     $module = $this->extractModule($event['SUMMARY']);
                     if ($module === null) {
@@ -28,11 +71,13 @@ class Contactmoment extends Controller
                 }
                 
                 // remove future, imported contactmomenten which where not touched in this batch (today)
-                \App\Contactmoment::where(function($query) {
+                \App\Contactmoment::where(function ($query) {
                     $query->where('updated_at', '<', date('Y-m-d'))->orWhereNull('updated_at');
-                })->where('starttijd', '>', date('Y-m-d'))->whereNotNull('ical_uid')->delete();
+                })->where('starttijd', '>', date('Y-m-d'))
+                    ->whereNotNull('ical_uid')
+                    ->delete();
                 break;
-                
+            
             case 'avansroosterjson':
                 foreach (json_decode(\Request::get("json"), true) as $event) {
                     $module = $this->extractModule($event['vak']);
@@ -47,15 +92,15 @@ class Contactmoment extends Controller
                     $uid = str_replace('-', '', $uid);
                     $uid = str_replace(' ', '', $uid);
                     $this->importEvent($module, new \DateTime($event['start']), new \DateTime($event['end']), $uid, $ruimte);
-                }   
+                }
                 break;
-                
+            
             default:
                 return abort(500, 'Unsupported import type');
         }
         return view("contactmoment.imported");
     }
-    
+
     private function extractModule(string $summary)
     {
         if (preg_match('/(?<module>[A-Z]+\d{1,2})/', $summary, $matches) !== 1) {
@@ -63,15 +108,17 @@ class Contactmoment extends Controller
         }
         return \App\Module::where('naam', $matches['module'])->first();
     }
-    
-    private function importEvent(\App\Module $module, \DateTime $starttijd, \DateTime $eindtijd, string $uid, string $ruimte) 
+
+    private function importEvent(\App\Module $module, \DateTime $starttijd, \DateTime $eindtijd, string $uid, string $ruimte)
     {
         $starttijd->setTimezone(new \DateTimeZone(ini_get('date.timezone')));
         $eindtijd->setTimezone(new \DateTimeZone(ini_get('date.timezone')));
         
         $contactmoment = \App\Contactmoment::where('ical_uid', $uid)->first();
         if ($contactmoment === null) {
-            $contactmoment = \App\Contactmoment::firstOrNew(['starttijd' => $starttijd]);
+            $contactmoment = \App\Contactmoment::firstOrNew([
+                'starttijd' => $starttijd
+            ]);
         }
         
         $contactmoment->ical_uid = $uid;
@@ -83,15 +130,14 @@ class Contactmoment extends Controller
             // try to find lesplan
             $lesplan = $module->lessen()->firstOrNew([
                 'jaar' => $contactmoment->starttijd->format('Y'),
-                'kalenderweek' => (int)$contactmoment->starttijd->format('W')
+                'kalenderweek' => (int) $contactmoment->starttijd->format('W')
             ]);
             $lesplan->doelgroep()->associate(\App\Doelgroep::find(1));
             $lesplan->save();
-        
+            
             $contactmoment->les()->associate($lesplan);
         }
         
         $contactmoment->save();
     }
-    
 }
