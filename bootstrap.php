715336ac4e175c9fca2a3596607c28741cb42a6c
Rik Meijer <rikmeijer@gmail.com>
2017-02-01 16:10:07 +0100

    use laravel .env as configuration source

--- bootstrap.php
+++ bootstrap.php
@@ -3,8 +3,13 @@ return function(string $factoryPath) {
 
     require __DIR__ . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 
-    $factory = require $factoryPath;
+    $dotenv = new Dotenv\Dotenv(__DIR__);
+    $dotenv->load();
 
-    return new \ActiveRecord\SQL\Schema($factory, new \PDO('mysql:dbname=teach', 'teach', 'teach', array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'')));
+    /**
+     * @var $factory \ActiveRecord\RecordFactory
+     */
+    $factory = require $factoryPath;
+    return new \ActiveRecord\SQL\Schema($factory, new \PDO($_ENV['DB_CONNECTION'] . ':dbname=' . $_ENV['DB_DATABASE'], $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\'')));
 
 };
