c8b78fb8553d39637f7dc305623c5e9bf392bbec
Rik Meijer <rikmeijer@gmail.com>
2017-03-13 15:29:59 +0100

    extend html5 layout

---
+++ resources/layouts/html5.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?= $this->harvest('title'); ?></title>
+    <?= $this->harvest('head'); ?>
+</head>
+<body><?= $this->harvest('body'); ?></body>
+</html>
--- resources/layouts/master.php
+++ resources/layouts/master.php
@@ -1,10 +1,6 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Teach - <?= $this->harvest('title'); ?></title>
-		<link rel="stylesheet" type="text/css" href="/css/lesplan.css">	
-    </head>
-    <body>
-        <?= $this->harvest('content'); ?>
-    </body>
-</html>
+<?php $layout = $this->layout('html5'); ?>
+<?php $layout->section('title', 'Teach - ' . html_entity_decode($this->harvest('title'))); ?>
+<?php $layout->section('head'); ?>
+<link rel="stylesheet" type="text/css" href="/css/lesplan.css">
+<?php $layout->section('body'); ?>
+<?= $this->harvest('content'); ?>
