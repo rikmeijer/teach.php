89f28f07123b4304ccd26a13514eadfe8fe9e8d1
Rik Meijer <rikmeijer@gmail.com>
2017-03-13 15:39:48 +0100

    use html5 layout for feedback template

--- resources/views/feedback.php
+++ resources/views/feedback.php
@@ -1,25 +1,23 @@
-<!DOCTYPE html>
-<html>
-<head>
+<?php $layout = $this->layout('html5'); ?>
+<?php $layout->section('title', 'Feedback'); ?>
+<?php $layout->section('head'); ?>
 <meta charset="utf-8">
 <meta http-equiv="refresh" content="5">
-<title>Feedback</title>
-
 <style>
-body {
-	font-family: arial, sans-serif;
-}
+    body {
+        font-family: arial, sans-serif;
+        text-align: center;
+    }
 </style>
-</head>
+
+<?php $layout->section('body'); ?>
 <body>
-	<center>
-		<h1>Feedback-o-meter</h1>
-        <?php $url = $this->url('/feedback/' . $contactmoment->id . '/supply'); ?>
-		<img src="<?=$this->url('/rating/' . rawurlencode($contactmoment->id)) ?>" width="500" height="100"
-			style="margin: 25px 0;" /></br> <img
-			src="<?=$this->url('/qr?data=' . rawurlencode($url)) ?>"
-			width="400" height="400" />
-		<p><?=$url?></p>
-	</center>
+<h1>Feedback-o-meter</h1>
+<?php $url = $this->url('/feedback/' . $contactmoment->id . '/supply'); ?>
+<img src="<?= $this->url('/rating/' . rawurlencode($contactmoment->id)) ?>" width="500" height="100"
+     style="margin: 25px 0;"/></br> <img
+        src="<?= $this->url('/qr?data=' . rawurlencode($url)) ?>"
+        width="400" height="400"/>
+<p><?= $url ?></p>
 </body>
 </html>
