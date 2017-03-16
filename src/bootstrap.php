f360173e2bfbce9054b22351b867f2ddbb6b3509
Rik Meijer <rikmeijer@gmail.com>
2017-03-16 17:08:03 +0100

    remove deprecated instance variables

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -6,26 +6,22 @@ return new class {
      */
     private $bootstrap;
 
-    /**
-     * @var \Aura\Router\Route
-     */
-    private $route;
-
-    /**
-     * @var \Psr\Http\Message\ServerRequestInterface
-     */
-    private $psrRequest;
 
     public function __construct()
     {
         $this->bootstrap = require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'bootstrap.php';
-        list($this->route, $this->psrRequest) = $this->bootstrap->match();
     }
 
     public function handle(callable $responseSender)
     {
-        if ($this->route !== false) {
-            $handler = $this->route->handler;
+        /**
+         * @var $route \Aura\Router\Route
+         * @var $psrRequest \Psr\Http\Message\ServerRequestInterface
+         */
+        list($route, $psrRequest) = $this->bootstrap->match();
+
+        if ($route !== false) {
+            $handler = $route->handler;
         } else {
             $handler = function (\rikmeijer\Teach\Resources $resources, \Psr\Http\Message\RequestInterface $request) : void {
                 $this->send(404, 'Failure');
@@ -34,6 +30,6 @@ return new class {
 
         $response = new \rikmeijer\Teach\Response($responseSender, $this->bootstrap->responseFactory());
         $handler = $response->bind($handler);
-        call_user_func($handler, $this->bootstrap->resources(), $this->psrRequest);
+        call_user_func($handler, $this->bootstrap->resources(), $psrRequest);
     }
 };
