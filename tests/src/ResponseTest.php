452062d25e478df1c2d7e9a28f9abb996b8d634f
Rik Meijer <rikmeijer@gmail.com>
2017-07-04 13:00:43 +0200

    add tests for Response factory

--- tests/src/ResponseTest.php
+++ tests/src/ResponseTest.php
@@ -11,8 +11,21 @@ namespace rikmeijer\Teach;
 class ResponseTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testSend_When_ValidStatusAndBody_Expect_ResponseBeingSend() {
+    public function testMake_When_StatusAndBodySupplied_Expect_PSRResponseWithStatusAndBody() {
 
+        $object = new Response();
+        $response = $object->make(200, 'HelloWorld');
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('HelloWorld', $response->getBody());
+    }
+
+    public function testMake_When_StatusAndBodyAndHeaderSupplied_Expect_PSRResponseWithStatusAndBodyAndHeader() {
+
+        $object = new Response();
+        $response = $object->makeWithHeaders(200, ['X-TEST' => 'Foobar'], 'HelloWorld');
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertEquals('HelloWorld', $response->getBody());
+        $this->assertEquals('Foobar', $response->getHeader('X-TEST')[0]);
     }
 
 }
