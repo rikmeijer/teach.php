91f2e103ccc034fcea22ac08e106d51ba457661d
Rik Meijer <rikmeijer@gmail.com>
2017-07-05 15:23:35 +0200

    fix improper break element

--- src/Routes/Feedback/views/feedback.php
+++ src/Routes/Feedback/views/feedback.php
@@ -15,7 +15,7 @@
 <h1>Feedback-o-meter</h1>
 <?php $url = $this->url('/feedback/' . $contactmoment->id . '/supply'); ?>
 <img src="<?= $this->url('/rating/%s', $contactmoment->id); ?>" width="500" height="100"
-     style="margin: 25px 0;"/></br> <img
+     style="margin: 25px 0;"/><br /> <img
         src="<?= $this->url('/qr?data=%s', $url); ?>"
         width="400" height="400"/>
 <p><?= $url; ?></p>
