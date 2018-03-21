bf212de240879611fca2886338e8a83d27aa9556
Rik Meijer <rikmeijer@gmail.com>
2018-03-21 21:00:02 +0100

    clean up database bootstrapping

--- resources/database/bootstrap.php
+++ resources/database/bootstrap.php
@@ -1,5 +1,4 @@
 <?php
-$config = require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'config.php';
-$pdo = new \PDO($config['DB_CONNECTION'] . ':', $config['DB_USERNAME'], $config['DB_PASSWORD'], array(\PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+$pdo = require __DIR__ . DIRECTORY_SEPARATOR . 'pdo.php';
 $connection = new pulledbits\ActiveRecord\SQL\Connection($pdo);
 return $connection->schema($config['DB_DATABASE']);
