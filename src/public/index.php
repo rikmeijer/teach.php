d756861ae3062567d7d45682ac91116d4ae5bcae
Rik Meijer <rikmeijer@gmail.com>
2018-11-26 11:58:26 +0100

    introduce caching to speed up response times

--- src/public/index.php
+++ src/public/index.php
@@ -1,12 +1,10 @@
 <?php
-
 $bootstrap = require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'application.php';
 $serverRequest = \GuzzleHttp\Psr7\ServerRequest::fromGlobals();
 $response = $bootstrap->handle($serverRequest);
-
-http_response_code($response->getStatusCode());
-foreach ($response->getHeaders() as $headerIdentifier => $headerValue) {
+http_response_code($response['status']);
+foreach ($response['headers'] as $headerIdentifier => $headerValue) {
     header($headerIdentifier . ': ' . implode(', ', $headerValue));
 }
-print $response->getBody();
+print $response['body'];
 exit;
