21dc8d25427d92244fd2a7d44f2ea83dc16131b1
Rik Meijer <rikmeijer@gmail.com>
2018-11-29 09:39:13 +0100

    add favicon

--- resources/phpview/layouts/master.php
+++ resources/phpview/layouts/master.php
@@ -2,6 +2,8 @@
 <?php $layout->section('title', 'Teach - ' . html_entity_decode($this->harvest('title'))); ?>
 <?php $layout->section('head'); ?>
 <link rel="stylesheet" type="text/css" href="/css/lesplan.css">
+<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+<link rel="icon" href="/favicon.ico" type="image/x-icon">
 <?php $layout->section('body'); ?>
 <?= $this->harvest('content'); ?>
 <?php $layout->compile(); ?>
