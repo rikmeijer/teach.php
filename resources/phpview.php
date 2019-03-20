a6e5ef68fd6c964f1e3f9af3ac24e08f08a08f1f
Rik Meijer <rikmeijer@gmail.com>
2019-03-20 11:46:54 +0100

    merge function into helper

--- resources/phpview.php
+++ resources/phpview.php
@@ -7,24 +7,6 @@
         $basedir . DIRECTORY_SEPARATOR . 'layouts'
     );
 
-    function get_absolute_path($path)
-    {
-        $path = str_replace(array('/', '\\'), DIRECTORY_SEPARATOR, $path);
-        $parts = array_filter(explode(DIRECTORY_SEPARATOR, $path), 'strlen');
-        $absolutes = array();
-        foreach ($parts as $part) {
-            if ('.' === $part) {
-                continue;
-            }
-            if ('..' === $part) {
-                array_pop($absolutes);
-            } else {
-                $absolutes[] = $part;
-            }
-        }
-        return implode(DIRECTORY_SEPARATOR, $absolutes);
-    }
-
     $directory->registerHelper(
         'url',
         function (string $path, string ...$unencoded): string {
@@ -33,7 +15,21 @@
                 $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) . '/' . $path;
             }
 
-            $path = sprintf(get_absolute_path($path), ...$encoded);
+            $path = str_replace('\\', '/', $path);
+            $parts = array_filter(explode('/', $path), 'strlen');
+            $absolutes = array();
+            foreach ($parts as $part) {
+                if ('.' === $part) {
+                    continue;
+                }
+                if ('..' === $part) {
+                    array_pop($absolutes);
+                } else {
+                    $absolutes[] = $part;
+                }
+            }
+
+            $path = sprintf(implode('/', $absolutes), ...$encoded);
             if (strpos($path, '?') === false) {
                 $query = '';
             } else {
