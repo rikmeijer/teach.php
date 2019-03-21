282e10b94e03b3ad91d58a3fb31546c140d16572
Rik Meijer <rikmeijer@gmail.com>
2019-03-21 10:26:41 +0100

    upgrade BaconQR

--- bootstrap/qr.php
+++ bootstrap/qr.php
@@ -1,13 +1,13 @@
-<?php return function (\pulledbits\Bootstrap\Bootstrap $bootstrap): \BaconQrCode\Renderer\Image\AbstractRenderer {
+<?php return function (\pulledbits\Bootstrap\Bootstrap $bootstrap): void {
     $bootstrap->resource('phpview')->registerHelper(
         'qr',
-        function (\pulledbits\View\TemplateInstance $templateInstance, int $width, int $height, string $data): void {
-            $renderer = new \BaconQrCode\Renderer\Image\Png();
-            $renderer->setHeight($width);
-            $renderer->setWidth($height);
+        function (\pulledbits\View\TemplateInstance $templateInstance, int $width, int $height, string $data) : void {
+            $renderer = new \BaconQrCode\Renderer\ImageRenderer(
+                new \BaconQrCode\Renderer\RendererStyle\RendererStyle($width),
+                new \BaconQrCode\Renderer\Image\ImagickImageBackEnd()
+            );
             $writer = new \BaconQrCode\Writer($renderer);
             print $writer->writeString($data);
         }
     );
-    return new \BaconQrCode\Renderer\Image\Png();
 };
