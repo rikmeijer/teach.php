78cd3ab555eaad0a4b3bd506f45733747e1c3f92
Rik Meijer <rikmeijer@gmail.com>
2019-03-21 09:42:10 +0100

    leverage tempalte resources in index

--- src/GUI/Index.php
+++ src/GUI/Index.php
@@ -5,14 +5,12 @@ namespace rikmeijer\Teach\GUI;
 
 use pulledbits\Router\Route;
 use pulledbits\View\Directory;
+use pulledbits\View\TemplateInstance;
 use rikmeijer\Teach\Contactmoment;
 use rikmeijer\Teach\GUI;
 
 final class Index implements GUI
 {
-    private $user;
-    private $schema;
-
     /**
      * @var Directory
      */
@@ -20,15 +18,41 @@ final class Index implements GUI
 
     public function __construct(\pulledbits\Bootstrap\Bootstrap $bootstrap)
     {
-        $this->user = $bootstrap->resource('user');
-        $this->schema = $bootstrap->resource('database');
         $this->phpviewDirectory = $bootstrap->resource('phpview');
+        $this->phpviewDirectory->registerHelper('modules', function(TemplateInstance $templateInstance) : array
+        {
+            $modules = [];
+            $userId = $templateInstance->resource('user')->uid;
+            $schema = $templateInstance->resource('database');
+            foreach ($schema->read('module', [], []) as $module) {
+                $modulecontactmomenten = Contactmoment::readByModuleName($schema, $userId, $module->naam);
 
-        $this->phpviewDirectory->registerHelper('modules', \Closure::fromCallable([$this, 'retrieveModules']));
-        $this->phpviewDirectory->registerHelper(
-            'contactmomenten',
-            \Closure::fromCallable([$this, 'retrieveContactmomenten'])
-        );
+                if (count($modulecontactmomenten) > 0) {
+                    $module->contains(['contactmomenten' => $modulecontactmomenten]);
+                    $module->bind(
+                        'retrieveRating',
+                        function () {
+                            $ratings = [];
+                            foreach ($this->contactmomenten as $modulecontactmoment) {
+                                $ratings[] = $modulecontactmoment->retrieveRating();
+                            }
+                            $numericRatings = array_filter($ratings, 'is_numeric');
+                            if (count($numericRatings) === 0) {
+                                return null;
+                            }
+                            return array_sum($numericRatings) / count($numericRatings);
+                        }
+                    );
+
+                    $modules[] = $module;
+                }
+            }
+            return $modules;
+        });
+        $this->phpviewDirectory->registerHelper('contactmomenten', function(TemplateInstance $templateInstance) : array
+        {
+            return Contactmoment::readVandaag($templateInstance->resource('database'), $templateInstance->resource('user')->uid);
+        });
     }
 
     public function addRoutesToRouter(\pulledbits\Router\Router $router): void
@@ -39,40 +63,5 @@ final class Index implements GUI
                 return new Index\Index($this->phpviewDirectory);
             }
         );
-    }
-
-    public function retrieveModules(): array
-    {
-        $modules = [];
-        $userId = $this->user->uid;
-        foreach ($this->schema->read('module', [], []) as $module) {
-            $modulecontactmomenten = Contactmoment::readByModuleName($this->schema, $userId, $module->naam);
-
-            if (count($modulecontactmomenten) > 0) {
-                $module->contains(['contactmomenten' => $modulecontactmomenten]);
-                $module->bind(
-                    'retrieveRating',
-                    function () {
-                        $ratings = [];
-                        foreach ($this->contactmomenten as $modulecontactmoment) {
-                            $ratings[] = $modulecontactmoment->retrieveRating();
-                        }
-                        $numericRatings = array_filter($ratings, 'is_numeric');
-                        if (count($numericRatings) === 0) {
-                            return null;
-                        }
-                        return array_sum($numericRatings) / count($numericRatings);
-                    }
-                );
-
-                $modules[] = $module;
-            }
-        }
-        return $modules;
-    }
-
-    public function retrieveContactmomenten(): array
-    {
-        return Contactmoment::readVandaag($this->schema, $this->user->uid);
     }
 }
