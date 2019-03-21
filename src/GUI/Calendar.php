d43a02c404bb153af0ada3c308542b9c83d93ca0
Rik Meijer <rikmeijer@gmail.com>
2019-03-21 09:35:12 +0100

    move method to closure

--- src/GUI/Calendar.php
+++ src/GUI/Calendar.php
@@ -17,40 +17,36 @@ final class Calendar implements GUI
 
     public function __construct(\pulledbits\Bootstrap\Bootstrap $bootstrap)
     {
-        $this->schema = $bootstrap->resource('database');
+        $schema = $bootstrap->resource('database');
         $this->phpviewDirectory = $bootstrap->resource('phpview');
-        $this->phpviewDirectory->registerHelper('calendar', function(\pulledbits\View\TemplateInstance $templateInstance, string $calendarIdentifier) : \Eluceo\iCal\Component\Calendar {
-           return $calendar = $this->retrieveCalendar($calendarIdentifier);
-        });
-    }
-
-    public function retrieveCalendar(string $calendarIdentifier): \Eluceo\iCal\Component\Calendar
-    {
-        $calendar = new \Eluceo\iCal\Component\Calendar($calendarIdentifier);
-        switch ($calendarIdentifier) {
-            case 'weeks':
-                $lesweken = $this->schema->read('lesweek', [], []);
-                foreach ($lesweken as $lesweek) {
-                    $lesweekEvent = new Event();
-                    $lesweekEvent->setNoTime(true);
-                    $lesweekEvent->setUniqueId(sha1($lesweek->jaar . $lesweek->kalenderweek));
-                    $lesweekEvent->setSummary('OW' . $lesweek->onderwijsweek . '/BW' . $lesweek->blokweek);
-                    try {
-                        $week_start = new \DateTime();
-                        $week_start->setISODate($lesweek->jaar, $lesweek->kalenderweek);
-                        $lesweekEvent->setDtStart($week_start);
-                        $lesweekEvent->setDtEnd($week_start);
-                        $calendar->addComponent($lesweekEvent);
-                    } catch (\Exception $e) {
+        $this->phpviewDirectory->registerHelper('calendar', function(\pulledbits\View\TemplateInstance $templateInstance, string $calendarIdentifier) use ($schema) : \Eluceo\iCal\Component\Calendar
+        {
+            $calendar = new \Eluceo\iCal\Component\Calendar($calendarIdentifier);
+            switch ($calendarIdentifier) {
+                case 'weeks':
+                    $lesweken = $schema->read('lesweek', [], []);
+                    foreach ($lesweken as $lesweek) {
+                        $lesweekEvent = new Event();
+                        $lesweekEvent->setNoTime(true);
+                        $lesweekEvent->setUniqueId(sha1($lesweek->jaar . $lesweek->kalenderweek));
+                        $lesweekEvent->setSummary('OW' . $lesweek->onderwijsweek . '/BW' . $lesweek->blokweek);
+                        try {
+                            $week_start = new \DateTime();
+                            $week_start->setISODate($lesweek->jaar, $lesweek->kalenderweek);
+                            $lesweekEvent->setDtStart($week_start);
+                            $lesweekEvent->setDtEnd($week_start);
+                            $calendar->addComponent($lesweekEvent);
+                        } catch (\Exception $e) {
+                        }
                     }
-                }
-                break;
+                    break;
 
-            default:
-                error_log('Unknown calendar ' . $calendarIdentifier . ' requested');
-                break;
-        }
-        return $calendar;
+                default:
+                    error_log('Unknown calendar ' . $calendarIdentifier . ' requested');
+                    break;
+            }
+            return $calendar;
+        });
     }
 
     public function addRoutesToRouter(\pulledbits\Router\Router $router): void
