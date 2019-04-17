5605c54d86f5f1c0485e24e48083984bc4aa22ae
Rik Meijer <rikmeijer@gmail.com>
2019-04-17 11:12:49 +0200

    use connection cofiguration

--- migrations-db.php
+++ migrations-db.php
@@ -2,7 +2,7 @@
 $config = (require __DIR__ . DIRECTORY_SEPARATOR . 'config.php')['DB'];
 
 return [
-    'driver' => 'pdo_mysql',
+    'driver' => 'pdo_' . $config['CONNECTION'],
     'host' => $config['HOST'],
     'dbname' => $config['DATABASE'],
     'user' => $config['USERNAME'],
