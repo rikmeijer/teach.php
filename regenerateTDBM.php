5e9498a697c842ac4995acdf24f4321557f97b2f
Rik Meijer <rikmeijer@gmail.com>
2019-04-18 14:05:12 +0200

    Abstracts are regenerated, normal Beans/DAOS will not be overwritten

--- regenerateTDBM.php
+++ regenerateTDBM.php
@@ -1,5 +1,8 @@
 <?php
 echo "Regenerating Daos and Beans...";
-$bootstrap = require __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
-$bootstrap->resource('tdbm')->generateAllDaosAndBeans();
+/**
+ * @var \TheCodingMachine\TDBM\TDBMService $tdbm
+ */
+$tdbm = (require __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php')->resource('tdbm');
+$tdbm->generateAllDaosAndBeans();
 echo 'OK' . PHP_EOL;
