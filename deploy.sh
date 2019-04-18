9d7c3242b0ff46f4d630f888f6826dc8f260eda2
Rik Meijer <rikmeijer@gmail.com>
2019-04-18 09:28:50 +0200

    provide deploy shell script for self-deployment

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
+git pull
+composer install --no-dev --working-dir=$SCRIPT_DIR --no-interaction --no-progress --no-suggest
+$SCRIPT_DIR/migrate-database
+php $SCRIPT_DIR/regenerateTDBM.php
--- regenerateTDBM.php
+++ regenerateTDBM.php
@@ -1,3 +1,5 @@
 <?php
+echo "Regenerating Daos and Beans...";
 $bootstrap = require __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap.php';
 $bootstrap->resource('tdbm')->generateAllDaosAndBeans();
+echo 'OK' . PHP_EOL;
