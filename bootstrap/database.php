9b76334917bb33a58478d25dfc0f74bd49c8aa67
Rik Meijer <rikmeijer@gmail.com>
2019-05-28 15:26:51 +0200

    remove pulledbits/activerecord

--- bootstrap/database.php
+++
@@ -1,7 +0,0 @@
-<?php return function (\pulledbits\Bootstrap\Bootstrap $bootstrap) {
-    $pdo = $bootstrap->resource('pdo');
-
-    $config = $bootstrap->config('DB');
-    $connection = new pulledbits\ActiveRecord\SQL\Connection($pdo);
-    return $connection->schema($config['DATABASE']);
-};
