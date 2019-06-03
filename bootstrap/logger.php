1633fac8b3a3bfb00971bc6a232d1f359dc23ba1
Rik Meijer <rikmeijer@gmail.com>
2019-06-03 15:25:08 +0200

    prevent error

--- bootstrap/logger.php
+++ bootstrap/logger.php
@@ -4,6 +4,6 @@ use Psr\Log\LoggerInterface;
 
 return function (\pulledbits\Bootstrap\Bootstrap $bootstrap) : LoggerInterface {
     $logger = new Monolog\Logger("pulledbits/teach");
-    $logger->pushHandler(new \Monolog\Handler\SyslogHandler("debug", ));
+    $logger->pushHandler(new \Monolog\Handler\SyslogHandler("debug"));
     return $logger;
 };
