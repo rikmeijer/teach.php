2bdb7af728f2c4dc8ab9ea7ed8753aab2acb4729
Rik Meijer <rikmeijer@gmail.com>
2019-06-04 14:39:34 +0200

    allow for environments variables to be imported into defaults config

--- config.defaults.php
+++ config.defaults.php
@@ -1,16 +1,27 @@
 <?php return [
-    'DB' => [ // used for Travis-CI
-        'CONNECTION' => 'mysql',
-        'HOST' => '127.0.0.1',
-        'PORT' => '3306',
-        'DATABASE' => 'teach',
-        'USERNAME' => 'travis',
-        'PASSWORD' => '',
-        'PREFIX' => '',
+    'DB' => [
+        'CONNECTION' => getenv('DB_CONNECTION'),
+        'HOST' => getenv('DB_HOST'),
+        'PORT' => getenv('DB_PORT'),
+        'DATABASE' => getenv('DB_DATABASE'),
+        'USERNAME' => getenv('DB_USERNAME'),
+        'PASSWORD' => getenv('DB_PASSWORD'),
+        'PREFIX' => getenv('DB_PREFIX')
     ],
+
+    'OAUTH' => [
+        'identifier' => getenv('OAUTH_CLIENT_ID'),
+        "secret" => getenv('OAUTH_CLIENT_SECRET'),
+        'callback_uri' => getenv('OAUTH_CALLBACK_URI')
+    ],
+
+    'MEMCACHED' => [
+        'host' => getenv('MEMCACHED_HOST'),
+        'port' => getenv('MEMCACHED_PORT')
+    ],
+
     'BOOTSTRAP' => [
-        'path' => __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap',
-        'preload' => []
+        'path' => __DIR__ . DIRECTORY_SEPARATOR . 'bootstrap'
     ],
     'ROUTER' => [
         'path' => __DIR__ . DIRECTORY_SEPARATOR . 'src' . DIRECTORY_SEPARATOR . 'GUI'
