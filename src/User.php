59883ada6a79df120da483aed4b255ab5e030e0e
Rik Meijer <rikmeijer@gmail.com>
2019-08-29 10:22:11 +0200

    cleanup

--- src/User.php
+++ src/User.php
@@ -2,9 +2,7 @@
 
 namespace rikmeijer\Teach;
 
-use Aura\Session\Session;
 use Auth0\SDK\Auth0;
-use Doctrine\DBAL\Connection;
 use pulledbits\Bootstrap\Bootstrap;
 use rikmeijer\Teach\Avans\Rooster;
 use rikmeijer\Teach\Beans\Module;
@@ -13,13 +11,6 @@ use TheCodingMachine\TDBM\ResultIterator;
 
 final class User
 {
-    /**
-     * @var Session
-     */
-    private $session;
-
-    private $oauth;
-
     /**
      * @var Auth0
      */
@@ -51,12 +42,14 @@ final class User
         return $useremailaddress->getUserid();
     }
 
-    public function login() : void {
+    public function login(): void
+    {
         $this->auth0->login();
     }
 
 
-    public function authorize() : void {
+    public function authorize(): void
+    {
         $details = $this->auth0->getUser();
         if ($details === null) {
 
@@ -67,7 +60,12 @@ final class User
     {
         $this->auth0->logout();
         $return_to = 'https://' . $_SERVER['HTTP_HOST'];
-        $logout_url = sprintf('https://%s/v2/logout?client_id=%s&returnTo=%s', 'pulledbits.eu.auth0.com', '2ohAli435Sq92PV14zh9vsXkFqofZrbh', $return_to);
+        $logout_url = sprintf(
+            'https://%s/v2/logout?client_id=%s&returnTo=%s',
+            'pulledbits.eu.auth0.com',
+            '2ohAli435Sq92PV14zh9vsXkFqofZrbh',
+            $return_to
+        );
         return $logout_url;
     }
 
@@ -76,13 +74,16 @@ final class User
         return $rooster->importEventsForUser($this->details()->getId());
     }
 
-    public function findContactmomentenToday() : ResultIterator
+    public function findContactmomentenToday(): ResultIterator
     {
         return ($this->daoFactory)('Contactmoment')->findContactmomentenTodayForUser($this->details()->getId());
     }
 
-    public function findContactmomentenByModule(Module $module) : ResultIterator
+    public function findContactmomentenByModule(Module $module): ResultIterator
     {
-        return ($this->daoFactory)('Contactmoment')->findContactmomentenForUserByModule($this->details()->getId(), $module);
+        return ($this->daoFactory)('Contactmoment')->findContactmomentenForUserByModule(
+            $this->details()->getId(),
+            $module
+        );
     }
 }
