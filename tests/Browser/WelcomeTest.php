12ac9877ff30171736f2e780e1347a4fe28b672c
Rik Meijer <rikmeijer@gmail.com>
2020-02-10 15:53:45 +0100

    rename test

--- tests/Browser/WelcomeTest.php
+++ tests/Browser/WelcomeTest.php
@@ -6,14 +6,14 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
 
-class ExampleTest extends DuskTestCase
+class WelcomeTest extends DuskTestCase
 {
     /**
      * A basic browser test example.
      *
      * @return void
      */
-    public function testBasicExample()
+    public function testSeeLiteralOverzichtContactmoment()
     {
         $this->browse(function (Browser $browser) {
             $browser->visit('/')
