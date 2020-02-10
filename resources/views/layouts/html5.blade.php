85d60aa50808122303e437a231c11c4789dfa1f9
Rik Meijer <rikmeijer@gmail.com>
2020-02-10 15:42:25 +0100

    start porting views by using dusk

---
+++ resources/views/layouts/html5.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Teach | @yield('title')</title>
+    @section('head')
+        Head
+    @show
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
+    <link rel="stylesheet" type="text/css" href="/css/teach.css">
+</head>
+<body>
+@yield('content')
+</body>
+</html>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.html5')
+@section('head')
+    <link rel="stylesheet" type="text/css" href="/css/lesplan.css">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+@endsection
