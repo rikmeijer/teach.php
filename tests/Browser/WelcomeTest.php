538f8aa89e059fcbcd54e6c5fbc6d70b87e59bb3
Rik Meijer <rik@rikmeijer.nl>
2020-02-14 10:31:56 +0100

    expect contactmomenten vandaag

--- tests/Browser/WelcomeTest.php
+++ tests/Browser/WelcomeTest.php
@@ -1,28 +1,22 @@
-<?php
+<?php /** @noinspection PhpUnhandledExceptionInspection */
+declare(strict_types=1);
 
 namespace Tests\Browser;
 
 use App\Module;
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Laravel\Dusk\Browser;
 use Tests\DuskTestCase;
-use Throwable;
 
 class WelcomeTest extends DuskTestCase
 {
-    use RefreshDatabase;
-
     final public function testSeeLiteralOverzichtContactmoment(): void
     {
-        try {
-            $this->browse(
-                static function (Browser $browser) {
-                    $browser->visit('/')
-                        ->assertSee('Overzicht contactmomenten');
-                }
-            );
-        } catch (Throwable $e) {
-        }
+        $this->browse(
+            static function (Browser $browser) {
+                $browser->visit('/')
+                    ->assertSee('Overzicht contactmomenten');
+            }
+        );
     }
 
     final public function testSeeAFSTModuleCodes(): void
@@ -33,31 +27,34 @@ class WelcomeTest extends DuskTestCase
                 'naam' => 'AFST'
             ]
         );
-        try {
-            $this->browse(
-                static function (Browser $browser) {
-                    $browser->visit('/')
-                        ->assertSeeLink('AFST');
-                }
-            );
-        } catch (Throwable $e) {
-        }
+        $this->browse(
+            static function (Browser $browser) {
+                $browser->visit('/')
+                    ->assertSeeLink('AFST');
+            }
+        );
     }
 
     final public function testSeeAllAvailableModuleCodes(): void
     {
-        try {
-            $this->browse(
-                static function (Browser $browser) {
-                    $page = $browser->visit('/');
-                    foreach (Module::all() as $module) {
-                        $page->assertSeeLink($module->naam);
-                    }
+        $this->browse(
+            static function (Browser $browser) {
+                $page = $browser->visit('/');
+                foreach (Module::all() as $module) {
+                    $page->assertSeeLink($module->naam);
                 }
-            );
-        } catch (Throwable $e) {
-            $this->assertTrue(false, $e->getMessage());
-        }
+            }
+        );
+    }
 
+
+    final public function testSeeContactmomentenVandaag(): void
+    {
+        $this->browse(
+            static function (Browser $browser) {
+                $browser->visit('/')
+                    ->assertSee('Contactmomenten vandaag');
+            }
+        );
     }
 }
