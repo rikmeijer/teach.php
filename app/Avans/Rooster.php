c68f07f4cc7002ae50528be9d37ef9ea167caa06
Rik Meijer <rik@rikmeijer.nl>
2020-02-20 14:35:20 +0100

    RED expect results from VObject\Reader

--- app/Avans/Rooster.php
+++ app/Avans/Rooster.php
@@ -3,9 +3,11 @@ declare(strict_types=1);
 
 namespace App\Avans;
 
+use Sabre\VObject\Document;
+
 class Rooster
 {
-    final public function import(): array
+    final public function import(Document $calendar): array
     {
         return [];
     }
--- tests/Unit/RoosterTest.php
+++ tests/Unit/RoosterTest.php
@@ -5,12 +5,34 @@ namespace Tests\Unit;
 
 use App\Avans\Rooster;
 use PHPUnit\Framework\TestCase;
+use Sabre\VObject\Reader;
 
 class RoosterTest extends TestCase
 {
+    private string $vobject;
+
+    final protected function setUp(): void
+    {
+        $this->vobject = <<<VOBJECT
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//Sabre//Sabre VObject 4.0.0-beta1//EN
+    CALSCALE:GREGORIAN
+BEGIN:VEVENT
+UID:sabre-vobject-2930d1fa-ac6d-42c8-92fe-06bb8bc3614e
+DTSTAMP:20150603T171911Z
+SUMMARY:Birthday party!
+    DTSTART;TZID=America/New_York:20160704T210000
+DTEND;TZID=America/New_York:20160705T030000
+END:VEVENT
+END:VCALENDAR
+VOBJECT;
+
+    }
+
     final public function testWhen_EventsImported_Expect_ListOfContactmomenten(): void
     {
         $object = new Rooster();
-        $this->assertCount(0, $object->import());
+        $this->assertCount(1, $object->import(Reader::read($this->vobject)));
     }
 }
